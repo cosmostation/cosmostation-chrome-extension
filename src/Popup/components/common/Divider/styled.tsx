288ff2b01710ba6f87a69d5e3eddb823bc2e90f9
ong-ar <reclusis@gmail.com>
2022-02-17 12:08:05 +0900

    change divider style

--- src/Popup/components/common/Divider/styled.tsx
+++ src/Popup/components/common/Divider/styled.tsx
@@ -2,5 +2,8 @@ import { styled } from '@mui/material/styles';
 
 export const StyledDivider = styled('hr')(({ theme }) => ({
   margin: 0,
-  border: `0.05rem solid ${theme.colors.base03}`,
+
+  border: 0,
+
+  borderTop: `0.1rem solid ${theme.colors.base03}`,
 }));
