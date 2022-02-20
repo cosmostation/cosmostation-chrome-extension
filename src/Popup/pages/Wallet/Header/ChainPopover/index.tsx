8c829d993db077007fb38716f4149eed85edf177
ong-ar <reclusis@gmail.com>
2022-02-20 15:55:16 +0900

    add logic to popover

--- src/Popup/pages/Wallet/Header/ChainPopover/index.tsx
+++ src/Popup/pages/Wallet/Header/ChainPopover/index.tsx
@@ -1,8 +1,11 @@
 import type { PopoverProps } from '@mui/material';
 import { Typography } from '@mui/material';
 
+import { COSMOS_CHAINS, ETHEREUM_CHAINS } from '~/constants/chain';
 import Divider from '~/Popup/components/common/Divider';
 import Popover from '~/Popup/components/common/Popover';
+import { useChromeStorage } from '~/Popup/hooks/useChromeStorage';
+import { useCurrent } from '~/Popup/hooks/useCurrent';
 import { useNavigate } from '~/Popup/hooks/useNavigate';
 
 import ChainItemButton from './ChainItemButton';
@@ -25,10 +28,18 @@ import SettingIcon from '~/images/icons/Setting.svg';
 
 type ChainPopoverProps = Omit<PopoverProps, 'children'>;
 
-export default function ChainPopover(props: ChainPopoverProps) {
+export default function ChainPopover({ onClose, ...remainder }: ChainPopoverProps) {
   const { navigate } = useNavigate();
+  const { currentAccount, currentChain } = useCurrent();
+  const { chromeStorage, setChromeStorage } = useChromeStorage();
+
+  const { allowedChains } = currentAccount;
+
+  const allowedCosmosChain = COSMOS_CHAINS.filter((chain) => allowedChains.includes(chain.id));
+  const allowedEthereumChain = ETHEREUM_CHAINS.filter((chain) => allowedChains.includes(chain.id));
+
   return (
-    <Popover {...props}>
+    <Popover {...remainder} onClose={onClose}>
       <Container>
         <HeaderContainer>
           <HeaderLeftContainer>
@@ -42,25 +53,45 @@ export default function ChainPopover(props: ChainPopoverProps) {
         </HeaderContainer>
         <Divider />
         <BodyContainer>
-          <TendermintChainListContainer>
-            <ChainListContainer>
-              <ChainItemButton isActive imgSrc="https://">
-                Cosmos
-              </ChainItemButton>
-              <ChainItemButton>Cosmos</ChainItemButton>
-              <ChainItemButton>cosmos</ChainItemButton>
-              <ChainItemButton>Cosmos</ChainItemButton>
-              <ChainItemButton>Cosmos</ChainItemButton>
-              <ChainItemButton>Cosmos</ChainItemButton>
-              <ChainItemButton>Cosmos</ChainItemButton>
-            </ChainListContainer>
-          </TendermintChainListContainer>
-          <Divider />
-          <EthereumChainListContainer>
-            <ChainListContainer>
-              <ChainItemButton>Ethereum</ChainItemButton>
-            </ChainListContainer>
-          </EthereumChainListContainer>
+          {allowedCosmosChain.length > 0 && (
+            <TendermintChainListContainer>
+              <ChainListContainer>
+                {allowedCosmosChain.map((chain) => (
+                  <ChainItemButton
+                    key={chain.id}
+                    isActive={currentChain.id === chain.id}
+                    imgSrc={chain.imageURL}
+                    onClick={async () => {
+                      await setChromeStorage('selectedChainId', { ...chromeStorage.selectedChainId, [currentAccount.id]: chain.id });
+                      onClose?.({}, 'backdropClick');
+                    }}
+                  >
+                    {chain.chainName}
+                  </ChainItemButton>
+                ))}
+              </ChainListContainer>
+            </TendermintChainListContainer>
+          )}
+          {allowedCosmosChain.length > 0 && allowedEthereumChain.length > 0 && <Divider />}
+          {allowedEthereumChain.length > 0 && (
+            <EthereumChainListContainer>
+              <ChainListContainer>
+                {allowedEthereumChain.map((chain) => (
+                  <ChainItemButton
+                    key={chain.id}
+                    isActive={currentChain.id === chain.id}
+                    imgSrc={chain.imageURL}
+                    onClick={async () => {
+                      await setChromeStorage('selectedChainId', { ...chromeStorage.selectedChainId, [currentAccount.id]: chain.id });
+                      onClose?.({}, 'backdropClick');
+                    }}
+                  >
+                    {chain.chainName}
+                  </ChainItemButton>
+                ))}
+              </ChainListContainer>
+            </EthereumChainListContainer>
+          )}
           <BetaChainContainer>
             <BetaChainTitleContainer>
               <Typography variant="h6">Beta support</Typography>
