45112763328671cbe580ffd049d6e4a85e84e308
ong-ar <reclusis@gmail.com>
2022-02-23 10:40:56 +0900

    add account management page

---
+++ src/Popup/pages/Account/Management/entry.tsx
@@ -0,0 +1,16 @@
+import MenuButton from '~/Popup/components/MenuButton';
+import { useNavigate } from '~/Popup/hooks/useNavigate';
+
+import { Container, ListContainer } from './styled';
+
+export default function Entry() {
+  const { navigate } = useNavigate();
+  return (
+    <Container>
+      <ListContainer>
+        <MenuButton onClick={() => navigate('/chain/management/use', { isDuplicateCheck: true })}>Chain management</MenuButton>
+        <MenuButton>Add Network</MenuButton>
+      </ListContainer>
+    </Container>
+  );
+}
