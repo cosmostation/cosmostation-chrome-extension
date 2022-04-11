b766d309e104ec81f00b727cd118633a9b46eb38
ong-ar <reclusis@gmail.com>
2022-04-11 17:50:35 +0900

    styling popover scrollbar

--- src/Popup/components/common/Popover/styled.tsx
+++ src/Popup/components/common/Popover/styled.tsx
@@ -2,6 +2,18 @@ import { Popover } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 export const StyledPopover = styled(Popover)(({ theme }) => ({
+  '*::-webkit-scrollbar': {
+    width: '0.1rem',
+    height: '0.1rem',
+    backgroundColor: 'transparent',
+  },
+  '*::-webkit-scrollbar-thumb': {
+    backgroundColor: theme.colors.base05,
+  },
+  '*::-webkit-scrollbar-corner': {
+    backgroundColor: 'transparent',
+  },
+
   '& .MuiPaper-root': {
     backgroundColor: theme.colors.base01,
 
--- src/constants/chain/index.ts
+++ src/constants/chain/index.ts
@@ -1,39 +1,39 @@
 import ethereumImg from '~/images/symbols/ethereum.png';
 import type { EthereumChain, EthereumNetwork, TendermintChain } from '~/types/chain';
 
-// import { AKASH } from './tendermint/akash';
+import { AKASH } from './tendermint/akash';
 import { AXELAR } from './tendermint/axelar';
-// import { BAND } from './tendermint/band';
-// import { BITCANNA } from './tendermint/bitcanna';
-// import { BITSONG } from './tendermint/bitsong';
-// import { CERBERUS } from './tendermint/cerberus';
-// import { CERTIK } from './tendermint/certik';
-// import { CHIHUAHUA } from './tendermint/chihuahua';
-// import { COMDEX } from './tendermint/comdex';
+import { BAND } from './tendermint/band';
+import { BITCANNA } from './tendermint/bitcanna';
+import { BITSONG } from './tendermint/bitsong';
+import { CERBERUS } from './tendermint/cerberus';
+import { CERTIK } from './tendermint/certik';
+import { CHIHUAHUA } from './tendermint/chihuahua';
+import { COMDEX } from './tendermint/comdex';
 import { COSMOS } from './tendermint/cosmos';
 // import { CRESCENT } from './tendermint/crescent';
-// import { CRYPTO_COM } from './tendermint/cryptoCom';
-// import { DESMOS } from './tendermint/desmos';
-// import { EMONEY } from './tendermint/emoney';
-// import { FETCH_AI } from './tendermint/fetchAi';
+import { CRYPTO_COM } from './tendermint/cryptoCom';
+import { DESMOS } from './tendermint/desmos';
+import { EMONEY } from './tendermint/emoney';
+import { FETCH_AI } from './tendermint/fetchAi';
 import { GRAVITY_BRIDGE } from './tendermint/gravityBridge';
 import { IRIS } from './tendermint/iris';
 import { JUNO } from './tendermint/juno';
 import { KAVA } from './tendermint/kava';
-// import { KI } from './tendermint/ki';
-// import { KONSTELLATION } from './tendermint/konstellation';
-// import { LUM } from './tendermint/lum';
-// import { MEDIBLOC } from './tendermint/medibloc';
-// import { OMNIFLIX } from './tendermint/omniflix';
+import { KI } from './tendermint/ki';
+import { KONSTELLATION } from './tendermint/konstellation';
+import { LUM } from './tendermint/lum';
+import { MEDIBLOC } from './tendermint/medibloc';
+import { OMNIFLIX } from './tendermint/omniflix';
 import { OSMOSIS } from './tendermint/osmosis';
-// import { PERSISTENCE } from './tendermint/persistence';
-// import { PROVENANCE } from './tendermint/provenance';
-// import { REGEN } from './tendermint/regen';
+import { PERSISTENCE } from './tendermint/persistence';
+import { PROVENANCE } from './tendermint/provenance';
+import { REGEN } from './tendermint/regen';
 import { RIZON } from './tendermint/rizon';
-// import { SENTINEL } from './tendermint/sentinel';
-// import { SIF } from './tendermint/sif';
+import { SENTINEL } from './tendermint/sentinel';
+import { SIF } from './tendermint/sif';
 import { STARGAZE } from './tendermint/stargaze';
-// import { STARNAME } from './tendermint/starname';
+import { STARNAME } from './tendermint/starname';
 import { UMEE } from './tendermint/umee';
 
 export const LINE_TYPE = {
@@ -59,30 +59,29 @@ export const TENDERMINT_CHAINS: TendermintChain[] = [
   GRAVITY_BRIDGE,
   RIZON,
   IRIS,
-  // CRESCENT,
-  // CHIHUAHUA,
-  // CERBERUS,
-  // CRYPTO_COM,
-  // BAND,
-  // AKASH,
-  // CERTIK,
-  // SENTINEL,
-  // PERSISTENCE,
-  // FETCH_AI,
-  // SIF,
-  // KI,
-  // STARNAME,
-  // MEDIBLOC,
-  // EMONEY,
-  // BITCANNA,
-  // BITSONG,
-  // COMDEX,
-  // DESMOS,
-  // KONSTELLATION,
-  // LUM,
-  // OMNIFLIX,
-  // PROVENANCE,
-  // REGEN,
+  CHIHUAHUA,
+  CERBERUS,
+  CRYPTO_COM,
+  BAND,
+  AKASH,
+  CERTIK,
+  SENTINEL,
+  PERSISTENCE,
+  FETCH_AI,
+  SIF,
+  KI,
+  STARNAME,
+  MEDIBLOC,
+  EMONEY,
+  BITCANNA,
+  BITSONG,
+  COMDEX,
+  DESMOS,
+  KONSTELLATION,
+  LUM,
+  OMNIFLIX,
+  PROVENANCE,
+  REGEN,
 ];
 
 export const ETHEREUM_CHAINS: EthereumChain[] = [
