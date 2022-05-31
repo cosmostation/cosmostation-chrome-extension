8c822600257981ebd2c21d267ccba2e3030b1eae
ong-ar <reclusis@gmail.com>
2022-05-31 14:23:56 +0900

    add versioning script

--- src/Popup/utils/axios.ts
+++ src/Popup/utils/axios.ts
@@ -1,11 +1,19 @@
 import axios from 'axios';
 
 export async function get<T>(path: string): Promise<T> {
-  const { data } = await axios.get<T>(path);
+  const { data } = await axios.get<T>(path, {
+    headers: {
+      Cosmostation: `extension/${String(process.env.VERSION)}`,
+    },
+  });
   return data;
 }
 
 export async function post<T>(path: string, body?: unknown): Promise<T> {
-  const { data } = await axios.post<T>(path, body);
+  const { data } = await axios.post<T>(path, body, {
+    headers: {
+      Cosmostation: `extension/${String(process.env.VERSION)}`,
+    },
+  });
   return data;
 }
