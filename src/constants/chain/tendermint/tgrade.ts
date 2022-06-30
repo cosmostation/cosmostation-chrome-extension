b0948f445eac2efbd87cf32eb20c48faa7a60d5a
Seo <reclusis@gmail.com>
2022-06-30 15:38:56 +0900

    add tgrade chain (#52)
    * add tgrade chain
    
    * change gas rate
    
    * remove explorer

---
+++ src/constants/chain/tendermint/tgrade.ts
@@ -0,0 +1,28 @@
+import tgradeImg from '~/images/symbols/tgrade.png';
+import type { TendermintChain } from '~/types/chain';
+
+export const TGRADE: TendermintChain = {
+  id: '8a4cc90b-fc6b-4fcb-b297-e4eddcbe8723',
+  line: 'TENDERMINT',
+  type: '',
+  chainId: 'tgrade-mainnet-1',
+  chainName: 'Tgrade',
+  restURL: 'https://lcd-tgrade.cosmostation.io',
+  imageURL: tgradeImg,
+  baseDenom: 'utgd',
+  displayDenom: 'TGD',
+  decimals: 6,
+  bip44: {
+    purpose: "44'",
+    coinType: "118'",
+    account: "0'",
+    change: '0',
+  },
+  bech32Prefix: { address: 'tgrade' },
+  gasRate: {
+    tiny: '0.005',
+    low: '0.0075',
+    average: '0.01',
+  },
+  gas: {},
+};
