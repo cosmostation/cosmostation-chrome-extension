ba129e97a072db774985d4864bdb0f1eb463d405
Seo <reclusis@gmail.com>
2022-08-18 10:44:42 +0900

    Add passage chain (#81)
    * Add passage chain
    
    * change gasRate

---
+++ src/constants/chain/cosmos/passage.ts
@@ -0,0 +1,30 @@
+import { MINTSCAN_URL } from '~/constants/common';
+import passageImg from '~/images/symbols/passage.png';
+import type { CosmosChain } from '~/types/chain';
+
+export const PASSAGE: CosmosChain = {
+  id: '551b0fe2-2fe1-458e-b801-13e1337cc09b',
+  line: 'COSMOS',
+  type: '',
+  chainId: 'passage-1',
+  chainName: 'Passage',
+  restURL: 'https://lcd-passage.cosmostation.io',
+  imageURL: passageImg,
+  baseDenom: 'upasg',
+  displayDenom: 'PASG',
+  decimals: 6,
+  bip44: {
+    purpose: "44'",
+    coinType: "118'",
+    account: "0'",
+    change: '0',
+  },
+  bech32Prefix: { address: 'pasg' },
+  explorerURL: `${MINTSCAN_URL}/passage`,
+  gasRate: {
+    tiny: '0',
+    low: '0.00025',
+    average: '0.0025',
+  },
+  gas: {},
+};
