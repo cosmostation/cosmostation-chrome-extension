9cdaafa39b89e97cb6c571a20f4a3a502d9c0257
Seo <reclusis@gmail.com>
2022-09-15 18:05:31 +0900

    add commelier chain (#89)

--- src/constants/chain/index.ts
+++ src/constants/chain/index.ts
@@ -39,6 +39,7 @@ import { SECRET } from './cosmos/secret';
 import { SENTINEL } from './cosmos/sentinel';
 import { SHENTU } from './cosmos/shentu';
 import { SIF } from './cosmos/sif';
+import { SOMMELIER } from './cosmos/sommelier';
 import { STARGAZE } from './cosmos/stargaze';
 import { STARNAME } from './cosmos/starname';
 import { TGRADE } from './cosmos/tgrade';
@@ -104,6 +105,7 @@ export const COSMOS_CHAINS: CosmosChain[] = [
   SENTINEL,
   SHENTU,
   SIF,
+  SOMMELIER,
   STARGAZE,
   STARNAME,
   TGRADE,
