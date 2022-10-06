0d6e09790f8adf2a330a49f001691f4fdf12fc31
SIhun Ahn <ahnsihun9208@gmail.com>
2022-10-06 15:57:39 +0900

    move ex to this branch

--- src/Popup/pages/Chain/Management/entry.tsx
+++ src/Popup/pages/Chain/Management/entry.tsx
@@ -18,6 +18,9 @@ export default function Entry() {
         <MenuButton onClick={() => navigate('/chain/ethereum/network/add', { isDuplicateCheck: true })}>
           {t('pages.Chain.Management.entry.addEthereumNetwork')}
         </MenuButton>
+        <MenuButton onClick={() => navigate('/chain/cosmos/token/add', { isDuplicateCheck: true })}>
+          {t('pages.Chain.Management.entry.addCosmosChain')}
+        </MenuButton>
         {/* <MenuButton>{t('pages.Chain.Management.entry.addChain')}</MenuButton> */}
       </ListContainer>
     </Container>
