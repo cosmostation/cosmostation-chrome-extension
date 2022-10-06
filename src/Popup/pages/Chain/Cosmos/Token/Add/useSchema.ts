3850fb296e1a62eef197e1b8d10e7e3de2e9655e
SIhun Ahn <ahnsihun9208@gmail.com>
2022-10-06 17:45:52 +0900

    editing schema: editing useSchema file in Chain/Add

--- src/Popup/pages/Chain/Cosmos/Token/Add/useSchema.ts
+++ src/Popup/pages/Chain/Cosmos/Token/Add/useSchema.ts
@@ -1,13 +1,15 @@
 import { useTranslation } from '~/Popup/hooks/useTranslation';
 import Joi from '~/Popup/utils/joi';
 import { hexRegex } from '~/Popup/utils/regex';
-import type { EthereumNetwork } from '~/types/chain';
+import type { EthereumNetwork, GasRate } from '~/types/chain';
+import type { CosAddChain } from '~/types/message/cosmos';
 
 export type AddNetworkForm = Pick<EthereumNetwork, 'chainId' | 'networkName' | 'rpcURL' | 'displayDenom' | 'explorerURL' | 'imageURL' | 'coinGeckoId'>;
+// export type AddChainForm = Pick<CosmosChain, 'chainId' | 'chainName' | 'restURL' |'baseDenom' |'coinType' | 'displayDenom' | 'explorerURL' | 'imageURL' | 'coinGeckoId'>;
 
 export function useSchema() {
   const { t } = useTranslation();
-
+  // TODO delete here
   const addNetworkForm = Joi.object<AddNetworkForm>({
     chainId: Joi.string()
       .required()
@@ -60,5 +62,61 @@ export function useSchema() {
       }),
   });
 
-  return { addNetworkForm };
+  // 받아야 하는 필드
+  //  addressPrefix,
+  // baseDenom,
+  // chainId,
+  // chainName,
+  // displayDenom,
+  // restURL,
+  // coinGeckoId,
+  // coinType,
+  // decimals,
+  // gasRate,
+  // imageURL,
+  // sendGas,
+  // type,
+  // cosmWasm,
+
+  const addChainForm = Joi.object<CosAddChain['params']>({
+    type: Joi.string()
+      // .valid(...cosmosType)
+      .default(''),
+    chainId: Joi.string()
+      .lowercase()
+      // .invalid(...officialChainIds)
+      .required(),
+    chainName: Joi.string()
+      .lowercase()
+      // .invalid(...invalidChainNames)
+      .required(),
+    restURL: Joi.string().required(),
+    imageURL: Joi.string().optional(),
+    baseDenom: Joi.string().required(),
+    displayDenom: Joi.string().required(),
+    decimals: Joi.number().optional(),
+    coinType: Joi.string()
+      .regex(/^[0-9]+$/)
+      .optional(),
+    addressPrefix: Joi.string().required(),
+    coinGeckoId: Joi.string().optional(),
+    gasRate: Joi.object<GasRate>({
+      tiny: Joi.string()
+        .required()
+        .regex(/^([0-9]+\.?[0-9]*|\.[0-9]+)$/),
+      low: Joi.string()
+        .required()
+        .regex(/^([0-9]+\.?[0-9]*|\.[0-9]+)$/),
+      average: Joi.string()
+        .required()
+        .regex(/^([0-9]+\.?[0-9]*|\.[0-9]+)$/),
+    }).optional(),
+    sendGas: Joi.string()
+      .regex(/^[0-9]+$/)
+      .optional(),
+    cosmWasm: Joi.boolean().optional(),
+  })
+    .label('params')
+    .required();
+  return { addNetworkForm, addChainForm };
 }
