b2d9c9b37d4003398b60664665d376115a80178c
SIhun Ahn <ahnsihun9208@gmail.com>
2022-10-07 18:26:18 +0900

    edit useSchema file: add .empty() and type input field

--- src/Popup/pages/Chain/Cosmos/Token/Add/useSchema.ts
+++ src/Popup/pages/Chain/Cosmos/Token/Add/useSchema.ts
@@ -1,4 +1,4 @@
-import { COSMOS_CHAINS } from '~/constants/chain';
+// import { COSMOS_CHAINS } from '~/constants/chain';
 import { COSMOS_TYPE } from '~/constants/cosmos';
 import { useTranslation } from '~/Popup/hooks/useTranslation';
 import Joi from '~/Popup/utils/joi';
@@ -11,16 +11,30 @@ export function useSchema() {
   const { t } = useTranslation();
 
   const cosmosType = Object.values(COSMOS_TYPE);
-  const officialCosmosLowercaseChainIds = COSMOS_CHAINS.map((item) => item.chainId.toLowerCase());
+  // const officialCosmosLowercaseChainIds = COSMOS_CHAINS.map((item) => item.chainId.toLowerCase());
 
+  // FIXME 현재 옵션값 미 입력시 undefine이 아닌 ''가 입력되어서 newChain할당시에 값이 ''가 들어감
   const addChainForm = Joi.object<CosAddChain['params']>({
+    // type: Joi.string()
+    //   .valid(...cosmosType)
+    //   .default(''),
     type: Joi.string()
+      .optional()
+      .allow('')
       .valid(...cosmosType)
-      .default(''),
+      .messages({
+        'string.base': t('schema.common.string.base'),
+        'string.empty': t('schema.common.string.empty'),
+      }),
     chainId: Joi.string()
-      .lowercase()
       .required()
-      .invalid(...officialCosmosLowercaseChainIds),
+      .lowercase()
+      // 위에서 강제로 소문자형변환 했음, 중복 체크는 뒤에서 하기위해 지움
+      // .invalid(...officialCosmosLowercaseChainIds)
+      .messages({
+        'string.base': t('schema.common.string.base'),
+        'string.empty': t('schema.common.string.empty'),
+      }),
     chainName: Joi.string()
       .required()
       .messages({
@@ -41,15 +55,14 @@ export function useSchema() {
       }),
     coinGeckoId: Joi.string()
       .optional()
-      .allow('')
+      .empty('')
       .messages({
         'string.base': t('schema.common.string.base'),
         'string.empty': t('schema.common.string.empty'),
       }),
-
     imageURL: Joi.string()
       .optional()
-      .allow('')
+      .empty('')
       .messages({
         'string.base': t('schema.common.string.base'),
         'string.empty': t('schema.common.string.empty'),
@@ -63,15 +76,18 @@ export function useSchema() {
     // decimals: Joi.number().optional(),
     decimals: Joi.string()
       .optional()
-      .allow('')
+      .empty('')
       .messages({
         'string.base': t('schema.common.string.base'),
         'string.empty': t('schema.common.string.empty'),
       }),
     coinType: Joi.string()
-      .regex(/^[0-9]+$/)
-      .allow('')
-      .optional(),
+      .optional()
+      .empty('')
+      .pattern(/^[0-9]+$/)
+      .messages({
+        'string.pattern.base': t('schema.addNetworkForm.address.string.pattern.base'),
+      }),
 
     addressPrefix: Joi.string()
       .required()
@@ -89,12 +105,17 @@ export function useSchema() {
       average: Joi.string()
         .required()
         .regex(/^([0-9]+\.?[0-9]*|\.[0-9]+)$/),
-    }).optional(),
+    })
+      .optional()
+      .empty(''),
     sendGas: Joi.string()
-      .regex(/^[0-9]+$/)
-      .allow('')
-      .optional(),
-    cosmWasm: Joi.boolean().optional(),
+      .optional()
+      .empty('')
+      .pattern(/^[0-9]+$/)
+      .messages({
+        'string.pattern.base': t('schema.common.number.base'),
+      }),
+    cosmWasm: Joi.boolean().optional().empty(''),
   });
   // .label('params')
   // .required();
