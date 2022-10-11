51208bb17ecab820c48302726708102dc8a9e211
SIhun Ahn <ahnsihun9208@gmail.com>
2022-10-11 15:34:36 +0900

    edit useSchema: activate gas rate input:not done

--- src/Popup/pages/Chain/Cosmos/Token/Add/useSchema.ts
+++ src/Popup/pages/Chain/Cosmos/Token/Add/useSchema.ts
@@ -3,9 +3,7 @@ import { COSMOS_TYPE } from '~/constants/cosmos';
 import { useTranslation } from '~/Popup/hooks/useTranslation';
 import Joi from '~/Popup/utils/joi';
 import type { GasRate } from '~/types/chain';
-import type { CosAddChain } from '~/types/message/cosmos';
-
-// export type AddChainForm = Pick<CosmosChain, 'chainId' | 'chainName' | 'restURL' |'baseDenom' |'coinType' | 'displayDenom' | 'explorerURL' | 'imageURL' | 'coinGeckoId'>;
+import type { CosAddChainParams } from '~/types/message/cosmos';
 
 export function useSchema() {
   const { t } = useTranslation();
@@ -14,23 +12,28 @@ export function useSchema() {
   // const officialCosmosLowercaseChainIds = COSMOS_CHAINS.map((item) => item.chainId.toLowerCase());
 
   // FIXME 현재 옵션값 미 입력시 undefine이 아닌 ''가 입력되어서 newChain할당시에 값이 ''가 들어감
-  const addChainForm = Joi.object<CosAddChain['params']>({
-    // type: Joi.string()
-    //   .valid(...cosmosType)
-    //   .default(''),
+  // -> .empty('')  엠티 필드를 오버라이딩함
+
+  // FIXME 가스 비율 인풋을 아예 비우거나 하나를 입력했을때는 모두 입력하도록 걸고 싶음
+  // -> .and() 로 해결한 듯
+  // FIXME 1개만 입력시 에러는 나는데 에러 메시지가 안나옴
+
+  // FIXME 가스레이트 값 미입력시 undefine으로 넘어가지 않음...
+
+  // TODO 가스 레이트에도 메시지 달아주기
+  const addChainForm = Joi.object<CosAddChainParams>({
     type: Joi.string()
       .optional()
-      .allow('')
+      .empty('')
       .valid(...cosmosType)
       .messages({
         'string.base': t('schema.common.string.base'),
         'string.empty': t('schema.common.string.empty'),
+        'any.only': t('schema.addChainForm.address.string.any.invalid'),
       }),
     chainId: Joi.string()
       .required()
       .lowercase()
-      // 위에서 강제로 소문자형변환 했음, 중복 체크는 뒤에서 하기위해 지움
-      // .invalid(...officialCosmosLowercaseChainIds)
       .messages({
         'string.base': t('schema.common.string.base'),
         'string.empty': t('schema.common.string.empty'),
@@ -43,19 +46,6 @@ export function useSchema() {
       }),
     restURL: Joi.string()
       .required()
-      .messages({
-        'string.base': t('schema.common.string.base'),
-        'string.empty': t('schema.common.string.empty'),
-      }),
-    displayDenom: Joi.string()
-      .required()
-      .messages({
-        'string.base': t('schema.common.string.base'),
-        'string.empty': t('schema.common.string.empty'),
-      }),
-    coinGeckoId: Joi.string()
-      .optional()
-      .empty('')
       .messages({
         'string.base': t('schema.common.string.base'),
         'string.empty': t('schema.common.string.empty'),
@@ -73,41 +63,68 @@ export function useSchema() {
         'string.base': t('schema.common.string.base'),
         'string.empty': t('schema.common.string.empty'),
       }),
-    // decimals: Joi.number().optional(),
-    decimals: Joi.string()
-      .optional()
-      .empty('')
+    displayDenom: Joi.string()
+      .required()
       .messages({
         'string.base': t('schema.common.string.base'),
         'string.empty': t('schema.common.string.empty'),
+      }),
+    decimals: Joi.number()
+      .optional()
+      .empty('')
+      .messages({
+        'number.base': t('schema.common.number.base'),
+        'number.empty': t('schema.common.number.empty'),
       }),
     coinType: Joi.string()
       .optional()
       .empty('')
       .pattern(/^[0-9]+$/)
       .messages({
-        'string.pattern.base': t('schema.addNetworkForm.address.string.pattern.base'),
+        'string.pattern.base': t('schema.common.number.base'),
       }),
-
     addressPrefix: Joi.string()
       .required()
       .messages({
         'string.base': t('schema.common.string.base'),
         'string.empty': t('schema.common.string.empty'),
       }),
+    coinGeckoId: Joi.string()
+      .optional()
+      .empty('')
+      .messages({
+        'string.base': t('schema.common.string.base'),
+        'string.empty': t('schema.common.string.empty'),
+      }),
     gasRate: Joi.object<GasRate>({
       tiny: Joi.string()
-        .required()
-        .regex(/^([0-9]+\.?[0-9]*|\.[0-9]+)$/),
+        .optional()
+        .empty('')
+        .regex(/^([0-9]+\.?[0-9]*|\.[0-9]+)$/)
+        .messages({
+          'string.pattern.base': t('schema.common.decimal.base'),
+        }),
       low: Joi.string()
-        .required()
-        .regex(/^([0-9]+\.?[0-9]*|\.[0-9]+)$/),
+        .optional()
+        .empty('')
+        .regex(/^([0-9]+\.?[0-9]*|\.[0-9]+)$/)
+        .messages({
+          'string.pattern.base': t('schema.common.decimal.base'),
+        }),
       average: Joi.string()
-        .required()
-        .regex(/^([0-9]+\.?[0-9]*|\.[0-9]+)$/),
+        .optional()
+        .empty('')
+        .regex(/^([0-9]+\.?[0-9]*|\.[0-9]+)$/)
+        .messages({
+          'string.pattern.base': t('schema.common.decimal.base'),
+        }),
     })
+      .and('tiny', 'low', 'average')
       .optional()
-      .empty(''),
+      .empty('')
+      .messages({
+        'object.and': t('schema.common.object.and'),
+      }),
     sendGas: Joi.string()
       .optional()
       .empty('')
@@ -115,10 +132,13 @@ export function useSchema() {
       .messages({
         'string.pattern.base': t('schema.common.number.base'),
       }),
-    cosmWasm: Joi.boolean().optional().empty(''),
+    cosmWasm: Joi.boolean()
+      .optional()
+      .empty('')
+      .messages({
+        'boolean.base': t('schema.common.boolean.base'),
+      }),
   });
-  // .label('params')
-  // .required();
 
   return { addChainForm };
 }
