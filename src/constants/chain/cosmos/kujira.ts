be57218a854b4740dccce67d03c98ebd73f7137c
seungeun-choi <112606995+seungeun-choi@users.noreply.github.com>
2022-10-21 11:28:40 +0900

    [WEB-970] Extension에 Kujira 체인 추가하기 (#100)

---
+++ src/constants/chain/cosmos/kujira.ts
@@ -0,0 +1,31 @@
+import { MINTSCAN_URL } from '~/constants/common';
+import kujiraImg from '~/images/symbols/kujira.png';
+import type { CosmosChain } from '~/types/chain';
+
+export const KUJIRA: CosmosChain = {
+  id: 'fa7a1907-662f-497c-95fb-2383e88341fd',
+  line: 'COSMOS',
+  type: '',
+  chainId: 'kaiyo-1',
+  chainName: 'Kujira',
+  restURL: 'https://lcd-kujira.cosmostation.io',
+  imageURL: kujiraImg,
+  baseDenom: 'ukuji',
+  displayDenom: 'KUJI',
+  decimals: 6,
+  bip44: {
+    purpose: "44'",
+    coinType: "118'",
+    account: "0'",
+    change: '0',
+  },
+  bech32Prefix: { address: 'kujira' },
+  coinGeckoId: 'kujira',
+  explorerURL: `${MINTSCAN_URL}/kujira`,
+  gasRate: {
+    tiny: '0.00119',
+    low: '0.00119',
+    average: '0.00119',
+  },
+  gas: { send: '100000' },
+};
