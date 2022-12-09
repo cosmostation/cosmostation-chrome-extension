a15618e5a4b8b8d941e912a801f367f4ea5735b9
ong-ar <reclusis@gmail.com>
2022-12-09 15:22:35 +0900

    Add useGasRateSWR

---
+++ src/Popup/hooks/SWR/cosmos/useGasRateSWR.ts
@@ -0,0 +1,83 @@
+import { useMemo } from 'react';
+import type { AxiosError } from 'axios';
+import type { SWRConfiguration } from 'swr';
+import useSWR from 'swr';
+
+import { get } from '~/Popup/utils/axios';
+import { convertCosmosToAssetName } from '~/Popup/utils/cosmos';
+import type { CosmosChain, GasRate } from '~/types/chain';
+import type { GasRateResponse } from '~/types/cosmos/gasRate';
+
+export function useGasRateSWR(chain: CosmosChain, config?: SWRConfiguration) {
+  const fetcher = async () => {
+    try {
+      return await get<GasRateResponse>('https://api.mintscan.io/v2/utils/params/gas_prices');
+    } catch {
+      return null;
+    }
+  };
+
+  const { data, error, mutate } = useSWR<GasRateResponse | null, AxiosError>({}, fetcher, {
+    revalidateOnFocus: false,
+    dedupingInterval: 14000,
+    refreshInterval: 0,
+    errorRetryCount: 0,
+    isPaused: () => !chain,
+    ...config,
+  });
+
+  const mappedName = convertCosmosToAssetName(chain);
+
+  const gasRate = useMemo(() => (data ? data.find((item) => item.chain === mappedName)?.rate ?? [] : []), [data, mappedName]);
+
+  const returnData: Record<string, GasRate> = useMemo(() => {
+    const result: Record<string, GasRate> = { [chain.baseDenom]: chain.gasRate };
+
+    gasRate.forEach((gr, idx) => {
+      const splitedItems = gr.split(',');
+
+      splitedItems.forEach((splitedItem) => {
+        const subIndex = splitedItem.search(/(?![0-9.])+/);
+
+        const rate = splitedItem.substring(0, subIndex);
+        const denom = splitedItem.substring(subIndex);
+
+        if (idx === 0) {
+          result[denom] = {
+            tiny: rate,
+            low: rate,
+            average: rate,
+          };
+        }
+
+        if (idx === 1) {
+          if (!result[denom]) {
+            result[denom] = {
+              tiny: rate,
+              low: rate,
+              average: rate,
+            };
+          } else {
+            result[denom] = { ...result[denom], low: rate, average: rate };
+          }
+        }
+
+        if (idx === 2) {
+          if (!result[denom]) {
+            result[denom] = {
+              tiny: rate,
+              low: rate,
+              average: rate,
+            };
+          } else {
+            result[denom] = { ...result[denom], average: rate };
+          }
+        }
+      });
+    });
+
+    return result;
+  }, [chain.baseDenom, chain.gasRate, gasRate]);
+
+  return { data: returnData, error, mutate };
+}
---
+++ src/types/cosmos/gasRate.ts
@@ -0,0 +1,7 @@
+export type GasRate = {
+  chain: string;
+  base: string;
+  rate: string[];
+};
+
+export type GasRateResponse = GasRate[];
