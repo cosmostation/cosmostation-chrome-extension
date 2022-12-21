9986997968aa2716a3a66495374ac2c02d30728a
ong-ar <reclusis@gmail.com>
2022-12-21 11:49:51 +0900

    Change aptos mainnet rpcurl

--- src/constants/chain/aptos/network/mainnet.ts
+++ src/constants/chain/aptos/network/mainnet.ts
@@ -5,7 +5,7 @@ export const MAINNET: AptosNetwork = {
   id: 'c163975b-c251-48f1-a257-a639cdac61f4',
   chainId: 1,
   networkName: 'Mainnet',
-  restURL: 'http://aptos.mainnet.cosmostation.io:8080',
+  restURL: 'https://rest-aptos.mainnet.cosmostation.io',
   imageURL: aptosImg,
   explorerURL: 'https://aptoscan.com',
   coinGeckoId: 'aptos',
