e94816a226558f5e9e45413579c6c993937c6d60
ong-ar <reclusis@gmail.com>
2023-02-02 19:21:22 +0900

    Change sui rpc url

--- src/constants/chain/sui/network/testnet.ts
+++ src/constants/chain/sui/network/testnet.ts
@@ -4,7 +4,7 @@ import type { SuiNetwork } from '~/types/chain';
 export const TESTNET: SuiNetwork = {
   id: '788aab81-6f84-4bc3-b47e-57a6a5ac0e32',
   networkName: 'Testnet',
-  rpcURL: 'https://fullnode.testnet.sui.io',
+  rpcURL: 'https://rpc-office.cosmostation.io/sui-testnet-wave-2',
   imageURL: suiImg,
   explorerURL: 'https://explorer.sui.io',
   displayDenom: 'SUI',
