c127ea6f729dbad8319a0974703ae71339b38338
Seo <reclusis@gmail.com>
2023-03-29 11:39:03 +0900

    Add quasar chain (#157)

--- src/constants/chain/index.ts
+++ src/constants/chain/index.ts
@@ -54,6 +54,7 @@ import { OSMOSIS } from './cosmos/osmosis';
 import { PASSAGE } from './cosmos/passage';
 import { PERSISTENCE } from './cosmos/persistence';
 import { PROVENANCE } from './cosmos/provenance';
+import { QUASAR } from './cosmos/quasar';
 import { QUICK_SILVER } from './cosmos/quickSilver';
 import { REGEN } from './cosmos/regen';
 import { RIZON } from './cosmos/rizon';
@@ -148,6 +149,7 @@ export const COSMOS_CHAINS: CosmosChain[] = [
   PASSAGE,
   PERSISTENCE,
   PROVENANCE,
+  QUASAR,
   QUICK_SILVER,
   REGEN,
   RIZON,
