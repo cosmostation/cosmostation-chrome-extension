6919d95417bd4bfb222290843fd1d5bcd3a66df2
G-Gamja <87967564+G-Gamja@users.noreply.github.com>
2023-04-05 22:17:43 +0900

    edit aptos&sui devnet Id

--- src/constants/chain/aptos/network/devnet.ts
+++ src/constants/chain/aptos/network/devnet.ts
@@ -2,7 +2,7 @@ import aptosImg from '~/images/symbols/aptos.png';
 import type { AptosNetwork } from '~/types/chain';
 
 export const DEVNET: AptosNetwork = {
-  id: '997a3322-ba19-4252-ac28-b9509a1bddcb',
+  id: '8efe46d5-c66c-4369-b704-65e90ee17dbe',
   chainId: 34,
   networkName: 'Devnet',
   restURL: 'https://fullnode.devnet.aptoslabs.com',
--- src/constants/chain/sui/network/devnet.ts
+++ src/constants/chain/sui/network/devnet.ts
@@ -2,7 +2,7 @@ import suiImg from '~/images/symbols/sui.png';
 import type { SuiNetwork } from '~/types/chain';
 
 export const DEVNET: SuiNetwork = {
-  id: '997a3322-ba19-4252-ac28-b9509a1bddcb',
+  id: '44d6259f-9382-4085-bd37-0be77226965b',
   networkName: 'Devnet',
   rpcURL: 'https://fullnode.devnet.sui.io',
   imageURL: suiImg,
