175cf085473602b2a395ff67cc02d6ede4a95ea7
ong-ar <reclusis@gmail.com>
2023-05-22 15:38:11 +0900

    add ledger 6.28.2 version

---
+++ src/Popup/hooks/useLedgerTransport6282.ts
@@ -0,0 +1,29 @@
+import { useRef } from 'react';
+import type Transport from '@ledgerhq/hw-transport-6282';
+
+import { createTransport62713 } from '~/Popup/utils/ledger';
+
+import { useChromeStorage } from './useChromeStorage';
+
+export function useLedgerTransport6282() {
+  const transport = useRef<Transport | undefined>(undefined);
+
+  const { chromeStorage } = useChromeStorage();
+
+  const { ledgerTransportType } = chromeStorage;
+
+  return {
+    transport: transport.current,
+    createTransport: async () => {
+      if (transport.current) {
+        return transport.current;
+      }
+      // eslint-disable-next-line no-return-assign
+      return (transport.current = await createTransport62713(ledgerTransportType));
+    },
+    closeTransport: async () => {
+      await transport.current?.close();
+      transport.current = undefined;
+    },
+  };
+}
--- src/Popup/utils/ledger/index.ts
+++ src/Popup/utils/ledger/index.ts
@@ -1,6 +1,8 @@
 // import TransportWebBle from '@ledgerhq/hw-transport-web-ble';
 import TransportWebHID from '@ledgerhq/hw-transport-webhid';
+import TransportWebHID62713 from '@ledgerhq/hw-transport-webhid-62713';
 import TransportWebUSB from '@ledgerhq/hw-transport-webusb';
+import TransportWebUSB62713 from '@ledgerhq/hw-transport-webusb-62713';
 
 import { TRANSPORT_TYPE } from '~/constants/ledger';
 import type { TransportType } from '~/types/ledger';
@@ -22,6 +24,25 @@ export async function createTransport(type: TransportType) {
 
   if ((await TransportWebHID.isSupported()) && type === TRANSPORT_TYPE.HID) {
     const transport = await TransportWebHID.create();
+    return transport;
+  }
+
+  // if ((await TransportWebBle.isSupported()) && type === TRANSPORT_TYPE.BLUETOOTH) {
+  //   const transport = await TransportWebBle.create();
+  //   return transport;
+  // }
+
+  throw new LedgerError(499, 'Not Supported');
+}
+
+export async function createTransport62713(type: TransportType) {
+  if ((await TransportWebUSB62713.isSupported()) && type === TRANSPORT_TYPE.USB) {
+    const transport = await TransportWebUSB62713.create();
+    return transport;
+  }
+
+  if ((await TransportWebHID62713.isSupported()) && type === TRANSPORT_TYPE.HID) {
+    const transport = await TransportWebHID62713.create();
     return transport;
   }
 
