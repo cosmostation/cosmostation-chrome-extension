cf3a41cc14db171a70f816ab8f2f687d12c1a50e
Sihun Ahn <87967564+G-Gamja@users.noreply.github.com>
2023-06-29 14:29:16 +0900

    [WEB-1820] 컨트랙트 토큰 기본 노출 (#205)

--- src/Popup/hooks/useCurrent/useCurrentCosmosTokens.ts
+++ src/Popup/hooks/useCurrent/useCurrentCosmosTokens.ts
@@ -2,20 +2,46 @@ import { useMemo } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import { useExtensionStorage } from '~/Popup/hooks/useExtensionStorage';
-import type { CosmosToken } from '~/types/chain';
+import type { CosmosChain, CosmosToken } from '~/types/chain';
 
 import { useCurrentChain } from './useCurrentChain';
+import { useTokensSWR } from '../SWR/cosmos/useTokensSWR';
 
 type AddCosmosTokenParams = Omit<CosmosToken, 'id'>;
 
-export function useCurrentCosmosTokens() {
+export function useCurrentCosmosTokens(chain: CosmosChain) {
   const { extensionStorage, setExtensionStorage } = useExtensionStorage();
 
   const { currentChain } = useCurrentChain();
+  const { data } = useTokensSWR(chain);
+
+  const defaultTokens: CosmosToken[] = useMemo(
+    () =>
+      data
+        .filter((item) => item.default)
+        .map((item) => ({
+          id: `${chain.chainId}${item.address}`,
+          address: item.address,
+          decimals: item.decimals,
+          displayDenom: item.symbol,
+          tokenType: 'CW20',
+          chainId: currentChain.id,
+          imageURL: item.image,
+          coinGeckoId: item.coinGeckoId,
+          default: item.default,
+        })),
+    [chain.chainId, currentChain.id, data],
+  );
 
   const { cosmosTokens } = extensionStorage;
 
-  const currentCosmosTokens = useMemo(() => cosmosTokens.filter((item) => item.chainId === currentChain.id), [cosmosTokens, currentChain.id]);
+  const currentCosmosTokens = useMemo(
+    () =>
+      [...defaultTokens, ...cosmosTokens.filter((item) => item.chainId === currentChain.id)].filter(
+        (token, idx, self) => self.findIndex((item) => item.address.toLowerCase() === token.address.toLowerCase()) === idx,
+      ),
+    [cosmosTokens, currentChain.id, defaultTokens],
+  );
 
   const addCosmosToken = async (token: AddCosmosTokenParams) => {
     const newCosmosTokens = [
