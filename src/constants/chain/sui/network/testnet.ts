2e026f2a28df2c0ad9703b6c8f7a7d5f47d6299c
Sihun Ahn <87967564+G-Gamja@users.noreply.github.com>
2023-07-19 16:23:28 +0900

    edit sui testnet rpc endpoint (#212)

--- src/constants/chain/sui/network/testnet.ts
+++ src/constants/chain/sui/network/testnet.ts
@@ -4,7 +4,7 @@ import type { SuiNetwork } from '~/types/chain';
 export const TESTNET: SuiNetwork = {
   id: '2f79310a-8621-49b6-b2d5-16b49f3605c1',
   networkName: 'Testnet',
-  rpcURL: 'https://rpc-sui-testnet.cosmostation.io/',
+  rpcURL: 'https://sui-testnet-kr-1.cosmostation.io',
   imageURL: suiImg,
   explorerURL: 'https://explorer.sui.io',
   displayDenom: 'SUI',
