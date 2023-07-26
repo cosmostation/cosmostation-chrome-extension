a8d0067c8f7e3f9cdc4ef259c3fc8f37cd944ba1
Seo <reclusis@gmail.com>
2023-07-26 18:00:40 +0900

    change webpack for firefox (#216)

--- webpack/webpack.prod.chrome.js
+++ webpack/webpack.prod.chrome.js
@@ -1,18 +1,11 @@
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
-const CopyPlugin = require('copy-webpack-plugin');
+const common = require('./webpack.common.chrome.js');
 const Dotenv = require('dotenv-webpack');
 const path = require('path');
 
 module.exports = merge(common, {
   mode: 'production',
-  plugins: [
-    new CopyPlugin({
-      patterns: [{ from: '.', to: '../', context: 'chrome' }],
-      options: {},
-    }),
-    new Dotenv({ path: './.env.chrome' }),
-  ],
+  plugins: [new Dotenv({ path: './.env.chrome' })],
   output: {
     path: path.join(__dirname, '../build/chrome/prod/js'),
     filename: '[name].js',
