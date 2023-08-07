b5e3678732a32423e54f3a5998d14e9ea332858a
Seo <reclusis@gmail.com>
2023-08-07 14:38:27 +0900

    remove autosign (#224)

--- src/Popup/pages/Popup/Cosmos/AutoSign/Delete/layout.tsx
+++
@@ -1,9 +0,0 @@
-import BaseLayout from '~/Popup/components/BaseLayout';
-
-type LayoutProps = {
-  children: JSX.Element;
-};
-
-export default function Layout({ children }: LayoutProps) {
-  return <BaseLayout>{children}</BaseLayout>;
-}
