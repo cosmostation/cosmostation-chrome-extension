298657b20ba563dfa21932e1cd6e640122f0d5d2
ong-ar <reclusis@gmail.com>
2023-08-10 18:37:01 +0900

    add getIpfsData

--- src/Popup/utils/nft.ts
+++ src/Popup/utils/nft.ts
@@ -1,3 +1,5 @@
+import Axios from 'axios';
+
 export function toDisplayTokenId(tokenId?: string) {
   if (!tokenId) return '';
 
@@ -12,4 +14,53 @@ export function convertIpfs(url?: string) {
 export function concatJsonFileType(url?: string) {
   if (!url) return '';
   return url.endsWith('.json') ? url : url.concat('.json');
+}
+
+const baseIpfsURL = 'https://ipfs.io/ipfs/';
+
+const axios = Axios.create({ baseURL: baseIpfsURL });
+
+export function getIpfsCID(ipfsUrl?: string) {
+  if (!ipfsUrl) return '';
+  return ipfsUrl.replace(/^ipfs:\/\// || /^ipfs:\//, '');
+}
+
+export function isIpfsUrl(url?: string) {
+  if (!url) return false;
+  return url.startsWith('ipfs://') || url.startsWith('ipfs:/');
+}
+
+export async function getIpfsData(CID: string): Promise<{ imageURL: string; metaData?: Record<string, unknown> } | null> {
+  try {
+    let response = await axios.get<Record<string, unknown>>(CID, { validateStatus: (status) => status < 500 });
+
+    if (response.status === 404 && !CID.endsWith('.json')) {
+      response = await axios.get(`${CID}.json`);
+    }
+
+    if (response.status >= 400) {
+      return null;
+    }
+
+    const isImage = response.headers['content-type'].startsWith('image');
+
+    if (isImage) {
+      return {
+        imageURL: `${baseIpfsURL}${CID}`,
+        metaData: undefined,
+      };
+    }
+
+    return {
+      imageURL:
+        typeof response.data.image === 'string'
+          ? isIpfsUrl(response.data.image)
+            ? `${baseIpfsURL}${getIpfsCID(response.data.image)}`
+            : response.data.image
+          : '',
+      metaData: response.data ?? undefined,
+    };
+  } catch {
+    return null;
+  }
 }
