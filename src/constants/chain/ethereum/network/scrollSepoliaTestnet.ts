3599b3d17685fb66ae1ecdeabe56d8fcb14ff015
Sihun Ahn <87967564+G-Gamja@users.noreply.github.com>
2023-09-15 11:51:36 +0900

    delete rpc url's last slash

--- src/constants/chain/ethereum/network/scrollSepoliaTestnet.ts
+++ src/constants/chain/ethereum/network/scrollSepoliaTestnet.ts
@@ -5,7 +5,7 @@ export const SCROLL_SEPOLIA_TESTNET: EthereumNetwork = {
   id: '6355cbed-4564-4fed-8557-758d67fbfffa',
   chainId: '0x8274f',
   networkName: 'Scroll Sepolia Testnet',
-  rpcURL: 'https://sepolia-rpc.scroll.io/',
+  rpcURL: 'https://sepolia-rpc.scroll.io',
   imageURL: scrollImg,
   displayDenom: 'ETH',
   decimals: 18,
