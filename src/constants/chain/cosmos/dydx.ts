db531739e2334f6e2e1219a3595248886c6120b5
ong-ar <reclusis@gmail.com>
2023-10-26 19:00:57 +0900

    Add dydx chain

---
+++ src/constants/chain/cosmos/dydx.ts
@@ -0,0 +1,31 @@
+import { MINTSCAN_URL } from '~/constants/common';
+import dydxImg from '~/images/symbols/dydx.png';
+import type { CosmosChain } from '~/types/chain';
+
+export const DYDX: CosmosChain = {
+  id: '70867501-f3ce-4b18-8467-a98de3b153be',
+  line: 'COSMOS',
+  type: '',
+  chainId: 'dydx-mainnet-1',
+  chainName: 'dYdX',
+  restURL: 'https://lcd-dydx.cosmostation.io',
+  imageURL: dydxImg,
+  baseDenom: 'adydx',
+  displayDenom: 'DYDX',
+  decimals: 18,
+  bip44: {
+    purpose: "44'",
+    coinType: "118'",
+    account: "0'",
+    change: '0',
+  },
+  bech32Prefix: { address: 'dydx' },
+  coinGeckoId: 'dydx',
+  explorerURL: `${MINTSCAN_URL}/dydx`,
+  gasRate: {
+    tiny: '10',
+    low: '50',
+    average: '100',
+  },
+  gas: { send: '100000' },
+};
