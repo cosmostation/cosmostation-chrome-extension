f24f1d307db918e1f2f169c2f2dea9b75d8afa25
SIhun Ahn <ahnsihun9208@gmail.com>
2024-01-02 12:18:38 +0900

    Edit params api response type

--- src/types/cosmos/params.ts
+++ src/types/cosmos/params.ts
@@ -12,10 +12,7 @@ export type GasPrice = {
 };
 
 export type Params = {
-  minting_inflation: MintingInflation;
-  staking_params: StakingParams;
   chainlist_params: ChainlistParams;
-  apr: string;
 };
 
 export type ChainlistParams = {
@@ -50,23 +47,4 @@ export type Fee = {
 export type GrpcEndpoint = {
   provider: string;
   url: string;
-};
-
-export type MintingInflation = {
-  inflation: string;
-};
-
-export type StakingParams = {
-  params: StakingParamsParams;
-};
-
-export type StakingParamsParams = {
-  unbonding_time: string;
-  max_validators: number;
-  max_entries: number;
-  historical_entries: number;
-  bond_denom: string;
-  validator_bond_factor: string;
-  global_liquid_staking_cap: string;
-  validator_liquid_staking_cap: string;
 };
