c4db9fddee8e3d35d8f51dd0169fffc3ec2d3253
SIhun Ahn <ahnsihun9208@gmail.com>
2024-01-25 14:46:44 +0900

    Remove unused import and update type definition in protoBuilder.ts

--- src/types/cosmos/protoBuilder.ts
+++ src/types/cosmos/protoBuilder.ts
@@ -1,8 +1,6 @@
 import type { cosmos } from '~/proto/cosmos-v0.44.2';
 
-import type { Msg } from './proto';
-
 export type ProtoBuilderDecodeResponse = {
   auth_info: cosmos.tx.v1beta1.AuthInfo;
-  body: cosmos.tx.v1beta1.TxBody & { messages: Msg[] };
+  body: cosmos.tx.v1beta1.TxBody;
 };
