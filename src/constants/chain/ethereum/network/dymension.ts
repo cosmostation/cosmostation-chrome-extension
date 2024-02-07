d13838ac881770a59e9d80b42fe2849512f95915
SIhun Ahn <ahnsihun9208@gmail.com>
2024-02-07 16:38:38 +0900

    Update chain constants for DYMENSION and ETHEREUM_NETWORK_DYMENSION

---
+++ src/constants/chain/ethereum/network/dymension.ts
@@ -0,0 +1,15 @@
+import { DYMENSION as COSMOS_DYMENSION } from '~/constants/chain/cosmos/dymension';
+import { MINTSCAN_URL } from '~/constants/common';
+import type { EthereumNetwork } from '~/types/chain';
+
+export const DYMENSION: EthereumNetwork = {
+  id: '1ee8fa80-ccaa-4046-8640-a4c740b5e003',
+  chainId: '0x44c',
+  networkName: COSMOS_DYMENSION.chainName,
+  rpcURL: 'https://rpc-dymension.cosmostation.io',
+  imageURL: COSMOS_DYMENSION.imageURL,
+  displayDenom: COSMOS_DYMENSION.displayDenom,
+  decimals: COSMOS_DYMENSION.decimals,
+  explorerURL: `${MINTSCAN_URL}/dymension`,
+  coinGeckoId: COSMOS_DYMENSION.coinGeckoId,
+};
