d0ee4aa79c7b0f9a9ea18adcfbacae516e559d08
SIhun Ahn <ahnsihun9208@gmail.com>
2024-03-05 14:49:06 +0900

    feat: Add sui useAmountSWR hook & apply accordion component
    - sui native체인카드에 아코디언 적용

--- src/Popup/hooks/SWR/sui/useDelegationSWR.ts
+++ src/Popup/hooks/SWR/sui/useDelegationSWR.ts
@@ -20,22 +20,31 @@ export function useDelegationSWR({ address, network }: UseDelegationSWR, config?
 
   const delegation = useMemo(
     () => ({
-      totalStakedAmount: suiStakes.data?.result?.reduce(
-        (allValidatorStakedSum, item) =>
-          plus(
-            allValidatorStakedSum,
-            item.stakes.reduce((eachValidatorStakedSum, stakeItem) => plus(eachValidatorStakedSum, stakeItem.principal), '0'),
-          ),
-        '0',
-      ),
+      totalStakedAmount:
+        suiStakes.data?.result?.reduce(
+          (allValidatorStakedSum, item) =>
+            plus(
+              allValidatorStakedSum,
+              item.stakes.reduce((eachValidatorStakedSum, stakeItem) => plus(eachValidatorStakedSum, stakeItem.principal), '0'),
+            ),
+          '0',
+        ) || '0',
+      totalEstimatedRewards:
+        suiStakes.data?.result?.reduce(
+          (allValidatorRewardsSum, item) =>
+            plus(
+              allValidatorRewardsSum,
+              item.stakes.reduce((eachValidatorRewardSum, stakeItem) => plus(eachValidatorRewardSum, stakeItem.estimatedReward || '0'), '0'),
+            ),
+          '0',
+        ) || '0',
       stakedObjects: suiStakes.data?.result?.map((stake) => ({
         ...stake,
-        validator: latestSuiSystemState.data?.result?.activeValidators.find((activeValidator) =>
-          isEqualsIgnoringCase(activeValidator.suiAddress, stake.validatorAddress),
-        ),
+        validator: latestSuiSystemState.data?.result?.activeValidators.find((validator) => isEqualsIgnoringCase(validator.suiAddress, stake.validatorAddress)),
       })),
     }),
     [suiStakes, latestSuiSystemState],
   );
+
   return { delegation };
 }
