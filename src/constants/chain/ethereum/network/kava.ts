5a73c7060d115d42780cc5a34d7ba342a3efe2a0
SIhun Ahn <ahnsihun9208@gmail.com>
2024-03-05 19:35:37 +0900

    feat: Change EVM rpc endpoints

--- src/constants/chain/ethereum/network/kava.ts
+++ src/constants/chain/ethereum/network/kava.ts
@@ -5,7 +5,7 @@ export const KAVA: EthereumNetwork = {
   id: '94c8a91b-258c-44ea-88b0-4e03b3257434',
   chainId: '0x8ae',
   networkName: 'Kava EVM',
-  rpcURL: 'https://evm.kava.io',
+  rpcURL: 'https://rpc-kava-evm.cosmostation.io',
   imageURL: COSMOS_KAVA.imageURL,
   displayDenom: COSMOS_KAVA.displayDenom,
   decimals: 18,
