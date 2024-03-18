bc83da1ed0bfc483f7601bf45735f541876837bc
SIhun Ahn <ahnsihun9208@gmail.com>
2024-03-18 17:21:06 +0900

    feat: Refactor calc eip 1559 gas estimations

--- src/Popup/hooks/SWR/ethereum/useFeeSWR.ts
+++ src/Popup/hooks/SWR/ethereum/useFeeSWR.ts
@@ -1,47 +1,21 @@
 import { useCallback, useMemo } from 'react';
 import type { SWRConfiguration } from 'swr';
 
+import { SMART_CHAIN } from '~/constants/chain/ethereum/network/smartChain';
+import { GAS_SETTINGS_BY_PRIORITY_LEVEL, PRIORITY_LEVEL } from '~/constants/ethereum';
 import { gt, medianOf, plus, times } from '~/Popup/utils/big';
-import type { FeeType } from '~/types/ethereum/common';
+import type { FeeType, GasEstimates } from '~/types/ethereum/common';
 
 import { useFeeHistorySWR } from './useFeeHistorySWR';
 import { useGasPriceSWR } from './useGasPriceSWR';
 import { useGetBlockByNumberSWR } from './useGetBlockByNumberSWR';
 import { useCurrentEthereumNetwork } from '../../useCurrent/useCurrentEthereumNetwork';
 
-const SETTINGS_BY_PRIORITY_LEVEL = {
-  tiny: {
-    baseFeePercentageMultiplier: 1.1,
-    priorityFeePercentageMultiplier: 0.94,
-    minMaxPriorityFeePerGas: '1000000000',
-  },
-  low: {
-    baseFeePercentageMultiplier: 1.2,
-    priorityFeePercentageMultiplier: 0.97,
-    minMaxPriorityFeePerGas: '1500000000',
-  },
-  average: {
-    baseFeePercentageMultiplier: 1.25,
-    priorityFeePercentageMultiplier: 0.98,
-    minMaxPriorityFeePerGas: '2000000000',
-  },
-};
-type GasFeeConfiguration = {
-  maxBaseFeePerGas: string;
-  maxPriorityFeePerGas: string;
-};
-
-type GasFeeEstimates = {
-  tiny: GasFeeConfiguration;
-  low: GasFeeConfiguration;
-  average: GasFeeConfiguration;
-};
-
 export function useFeeSWR(config?: SWRConfiguration) {
   const { currentEthereumNetwork } = useCurrentEthereumNetwork();
 
-  const block = useGetBlockByNumberSWR(['pending', false], config);
-  const feeHistory = useFeeHistorySWR([5, 'pending', [10, 20, 30]], config);
+  const block = useGetBlockByNumberSWR(['latest', false], config);
+  const feeHistory = useFeeHistorySWR([5, 'latest', [10, 20, 30]], config);
   const gasPrice = useGasPriceSWR(config);
 
   const currentGasPrice = gasPrice.data?.result ? plus(parseInt(gasPrice.data.result, 16), '1000') : null;
@@ -59,59 +33,26 @@ export function useFeeSWR(config?: SWRConfiguration) {
 
     const medianRewards = transposedRewards.map((item) => medianOf(item.map((reward) => parseInt(reward, 16))));
 
-    return medianRewards.reduce((acc: GasFeeEstimates, item, i) => {
-      if (i === 0) {
-        const { minMaxPriorityFeePerGas, priorityFeePercentageMultiplier, baseFeePercentageMultiplier } = SETTINGS_BY_PRIORITY_LEVEL.tiny;
+    const priorityLevels = Object.values(PRIORITY_LEVEL);
 
-        const feeWithPriorityMultiplier = times(item, priorityFeePercentageMultiplier);
-        const maxPriorityFeePerGas = gt(feeWithPriorityMultiplier, minMaxPriorityFeePerGas) ? feeWithPriorityMultiplier : minMaxPriorityFeePerGas;
+    const rewardsWithLevels = medianRewards.map((reward, i) => ({ reward, level: priorityLevels[i] }));
 
-        const maxBaseFeePerGas = plus(times(baseFeePerGas, baseFeePercentageMultiplier), maxPriorityFeePerGas);
+    return rewardsWithLevels.reduce((acc: GasEstimates, { reward, level }) => {
+      const { minMaxPriorityFeePerGas, priorityFeePercentageMultiplier, baseFeePercentageMultiplier } = GAS_SETTINGS_BY_PRIORITY_LEVEL[level];
 
-        return {
-          ...acc,
-          tiny: {
-            maxBaseFeePerGas,
-            maxPriorityFeePerGas,
-          },
-        };
-      }
+      const multipliedPriorityFee = times(reward, priorityFeePercentageMultiplier, 0);
+      const maxPriorityFeePerGas = gt(multipliedPriorityFee, minMaxPriorityFeePerGas) ? multipliedPriorityFee : minMaxPriorityFeePerGas;
 
-      if (i === 1) {
-        const { minMaxPriorityFeePerGas, priorityFeePercentageMultiplier, baseFeePercentageMultiplier } = SETTINGS_BY_PRIORITY_LEVEL.low;
+      const maxBaseFeePerGas = plus(times(baseFeePerGas, baseFeePercentageMultiplier, 0), maxPriorityFeePerGas);
 
-        const feeWithPriorityMultiplier = times(item, priorityFeePercentageMultiplier);
-        const maxPriorityFeePerGas = gt(feeWithPriorityMultiplier, minMaxPriorityFeePerGas) ? feeWithPriorityMultiplier : minMaxPriorityFeePerGas;
-
-        const maxBaseFeePerGas = plus(times(baseFeePerGas, baseFeePercentageMultiplier), maxPriorityFeePerGas);
-
-        return {
-          ...acc,
-          low: {
-            maxBaseFeePerGas,
-            maxPriorityFeePerGas,
-          },
-        };
-      }
-
-      if (i === 2) {
-        const { minMaxPriorityFeePerGas, priorityFeePercentageMultiplier, baseFeePercentageMultiplier } = SETTINGS_BY_PRIORITY_LEVEL.average;
-
-        const feeWithPriorityMultiplier = times(item, priorityFeePercentageMultiplier);
-        const maxPriorityFeePerGas = gt(feeWithPriorityMultiplier, minMaxPriorityFeePerGas) ? feeWithPriorityMultiplier : minMaxPriorityFeePerGas;
-
-        const maxBaseFeePerGas = plus(times(baseFeePerGas, baseFeePercentageMultiplier), maxPriorityFeePerGas);
-
-        return {
-          ...acc,
-          average: {
-            maxBaseFeePerGas,
-            maxPriorityFeePerGas,
-          },
-        };
-      }
-      return acc;
-    }, {} as GasFeeEstimates);
+      return {
+        ...acc,
+        [level]: {
+          maxBaseFeePerGas,
+          maxPriorityFeePerGas,
+        },
+      };
+    }, {} as GasEstimates);
   }, [feeHistory, block]);
 
   const type: FeeType | null = (() => {
@@ -119,7 +60,7 @@ export function useFeeSWR(config?: SWRConfiguration) {
       return null;
     }
 
-    const basicFeeOnlyChainIds: string[] = [];
+    const basicFeeOnlyChainIds = [SMART_CHAIN.id];
 
     if (basicFeeOnlyChainIds.includes(currentEthereumNetwork.id)) {
       return 'BASIC';
