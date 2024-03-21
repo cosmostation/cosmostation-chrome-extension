18ca8a4162c3dc647f511e6c91b86ff3386fc898
SIhun Ahn <ahnsihun9208@gmail.com>
2024-03-21 16:24:43 +0900

    useChainIdToAssetNameMapsSWR 함수에서 chain_id가 누락된 경우에 대한 처리 추가

--- src/Popup/hooks/SWR/cosmos/useChainIdToAssetNameMapsSWR.ts
+++ src/Popup/hooks/SWR/cosmos/useChainIdToAssetNameMapsSWR.ts
@@ -9,7 +9,8 @@ export function useChainIdToAssetNameMapsSWR(config?: SWRConfiguration) {
   const allParams = useAllParamsSWR(config);
 
   const chainIdToAssetNameMaps = useMemo<ChainIdToAssetNameMapsResponse>(
-    () => Object.fromEntries(Object.entries(allParams.data).map(([key, value]) => [value.params?.chainlist_params?.chain_id_cosmos || value.chain_id, key])),
+    () =>
+      Object.fromEntries(Object.entries(allParams.data).map(([key, value]) => [value.params?.chainlist_params?.chain_id_cosmos || value.chain_id || '', key])),
     [allParams],
   );
 
