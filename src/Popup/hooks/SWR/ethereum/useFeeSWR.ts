2e1fb3cb335c2bfeb8797b1d92b027f463efac3f
SIhun Ahn <ahnsihun9208@gmail.com>
2024-04-09 12:12:41 +0900

    feat: Apply ios eip-1559 gas estimation logic

--- src/Popup/hooks/SWR/ethereum/useFeeSWR.ts
+++ src/Popup/hooks/SWR/ethereum/useFeeSWR.ts
@@ -2,32 +2,38 @@ import { useCallback, useMemo } from 'react';
 import type { SWRConfiguration } from 'swr';
 
 import { SMART_CHAIN } from '~/constants/chain/ethereum/network/smartChain';
+import { GAS_SETTINGS_BY_PRIORITY_LEVEL } from '~/constants/ethereum';
 import { calculatePercentiles, divide, gt, plus } from '~/Popup/utils/big';
-import type { FeeType } from '~/types/ethereum/common';
+import type { GasRateKey } from '~/types/chain';
+import type { FeeType, GasEstimates } from '~/types/ethereum/common';
 
 import { useFeeHistorySWR } from './useFeeHistorySWR';
 import { useGasPriceSWR } from './useGasPriceSWR';
 import { useCurrentEthereumNetwork } from '../../useCurrent/useCurrentEthereumNetwork';
 
+const REWARD_PERCENTILES = [25, 50, 75];
+
+const BLOCK_COUNT = 20;
+
 export function useFeeSWR(config?: SWRConfiguration) {
   const { currentEthereumNetwork } = useCurrentEthereumNetwork();
 
-  const feeHistory = useFeeHistorySWR([20, 'latest', [25, 50, 75]], config);
+  const feeHistory = useFeeHistorySWR([BLOCK_COUNT, 'latest', REWARD_PERCENTILES], config);
 
   const gasPrice = useGasPriceSWR(config);
 
   const currentGasPrice = gasPrice.data?.result ? plus(parseInt(gasPrice.data.result, 16), '1000') : null;
 
   const currentFee = useMemo(() => {
-    if (!feeHistory.data?.result || !feeHistory.data.result.baseFeePerGas.every((item) => item !== null)) {
+    if (!feeHistory.data?.result || feeHistory.data.result.baseFeePerGas.some((item) => item === null)) {
       return null;
     }
 
     const { baseFeePerGas } = feeHistory.data.result;
 
-    const gasFeePercentiles = calculatePercentiles(
+    const baseFeePercentiles = calculatePercentiles(
       baseFeePerGas.map((item) => parseInt(item || '0', 16)),
-      [25, 50, 75],
+      REWARD_PERCENTILES,
     );
 
     const originReward = feeHistory.data.result.reward || [];
@@ -37,29 +43,24 @@ export function useFeeSWR(config?: SWRConfiguration) {
       .reduce((prev, cur) => [plus(prev[0], parseInt(cur[0], 16)), plus(prev[1], parseInt(cur[1], 16)), plus(prev[2], parseInt(cur[2], 16))], ['0', '0', '0'])
       .map((item) => divide(item, rewardCount, 0));
 
-    return {
-      tiny: {
-        maxBaseFeePerGas: plus(
-          gasFeePercentiles[0] && gt(gasFeePercentiles[0], '500000000') ? gasFeePercentiles[0] : '500000000',
-          averageReward[0] && gt(averageReward[0], '1000000000') ? averageReward[0] : '1000000000',
-        ),
-        maxPriorityFeePerGas: averageReward[0] && gt(averageReward[0], '1000000000') ? averageReward[0] : '1000000000',
-      },
-      low: {
-        maxBaseFeePerGas: plus(
-          gasFeePercentiles[1] && gt(gasFeePercentiles[1], '500000000') ? gasFeePercentiles[1] : '500000000',
-          averageReward[1] && gt(averageReward[1], '1000000000') ? averageReward[1] : '1000000000',
-        ),
-        maxPriorityFeePerGas: averageReward[1] && gt(averageReward[1], '1000000000') ? averageReward[1] : '1000000000',
-      },
-      average: {
-        maxBaseFeePerGas: plus(
-          gasFeePercentiles[2] && gt(gasFeePercentiles[2], '500000000') ? gasFeePercentiles[2] : '500000000',
-          averageReward[2] && gt(averageReward[2], '1000000000') ? averageReward[2] : '1000000000',
-        ),
-        maxPriorityFeePerGas: averageReward[2] && gt(averageReward[2], '1000000000') ? averageReward[2] : '1000000000',
-      },
-    };
+    return (['tiny', 'low', 'average'] as GasRateKey[]).reduce((acc: GasEstimates, level, index) => {
+      const { minBaseFeePerGas, minMaxPriorityFeePerGas } = GAS_SETTINGS_BY_PRIORITY_LEVEL[level];
+
+      const maxPriorityFeePerGas = averageReward[index] && gt(averageReward[index], minMaxPriorityFeePerGas) ? averageReward[index] : minMaxPriorityFeePerGas;
+
+      const maxBaseFeePerGas = plus(
+        baseFeePercentiles[index] && gt(baseFeePercentiles[index], minBaseFeePerGas) ? baseFeePercentiles[index] : minBaseFeePerGas,
+        maxPriorityFeePerGas,
+      );
+
+      return {
+        ...acc,
+        [level]: {
+          maxBaseFeePerGas,
+          maxPriorityFeePerGas,
+        },
+      };
+    }, {} as GasEstimates);
   }, [feeHistory]);
 
   const type: FeeType | null = (() => {
