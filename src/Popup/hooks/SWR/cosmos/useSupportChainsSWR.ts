6e316bcb37d729485c7885342980567bd08f9fde
Sihun Ahn <87967564+G-Gamja@users.noreply.github.com>
2024-04-24 17:08:25 +0900

    [APIS-269] Migrate mintscan v1, v3 api to v10 (#314)

--- src/Popup/hooks/SWR/cosmos/useSupportChainsSWR.ts
+++ src/Popup/hooks/SWR/cosmos/useSupportChainsSWR.ts
@@ -2,11 +2,12 @@ import type { AxiosError } from 'axios';
 import type { SWRConfiguration } from 'swr';
 import useSWR from 'swr';
 
+import { MINTSCAN_FRONT_API_URL } from '~/constants/common';
 import { get } from '~/Popup/utils/axios';
 import type { SupportChainPayload } from '~/types/cosmos/supportChains';
 
 export function useSupportChainsSWR(config?: SWRConfiguration) {
-  const requestURL = `https://front.api.mintscan.io/v1/meta/support/chains`;
+  const requestURL = `${MINTSCAN_FRONT_API_URL}/meta/support/chains`;
 
   const fetcher = (fetchUrl: string) => get<SupportChainPayload>(fetchUrl);
 
