5cd410b5daed3e4c51d2e184da56f2635c40c0d5
SIhun Ahn <ahnsihun9208@gmail.com>
2024-04-24 16:51:36 +0900

    feat: Revert removed eip6963 events type

--- src/types/ethereum/eip6963.ts
+++ src/types/ethereum/eip6963.ts
@@ -9,3 +9,12 @@ export type EIP6963ProviderDetail = {
   info: EIP6963ProviderInfo;
   provider: Ethereum;
 };
+
+export type EIP6963AnnounceProviderEvent = CustomEvent & {
+  type: 'eip6963:announceProvider';
+  detail: EIP6963ProviderDetail;
+};
+
+export type EIP6963RequestProviderEvent = Event & {
+  type: 'eip6963:requestProvider';
+};
