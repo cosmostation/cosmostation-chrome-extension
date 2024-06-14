588028ea414182072bf35ed2521e755447c3d796
Sihun Ahn <87967564+G-Gamja@users.noreply.github.com>
2024-06-14 13:44:12 +0900

    [APIS-319] Sort Aptos coin list (#345)

--- src/Popup/hooks/useCurrent/useCurrentAptosCoins.ts
+++ src/Popup/hooks/useCurrent/useCurrentAptosCoins.ts
@@ -1,6 +1,9 @@
+import { useMemo } from 'react';
 import type { SWRConfiguration } from 'swr';
 
-import { ACCOUNT_TYPE } from '~/constants/aptos';
+import { ACCOUNT_TYPE, APTOS_COIN } from '~/constants/aptos';
+import { getCoinAddress } from '~/Popup/utils/aptos';
+import { isEqualsIgnoringCase } from '~/Popup/utils/string';
 import type { X1CoinCoinstore } from '~/types/aptos/accounts';
 
 import { useCurrentAptosNetwork } from './useCurrentAptosNetwork';
@@ -11,7 +14,13 @@ export function useCurrentAptosCoins(config?: SWRConfiguration) {
 
   const accountResources = useAccountResourcesSWR({ network: currentAptosNetwork }, config);
 
-  const currentAptosCoins = (accountResources.data?.filter((item) => item.type.startsWith(ACCOUNT_TYPE.X1___COIN___COIN_STORE)) || []) as X1CoinCoinstore[];
+  const currentAptosCoins = useMemo(
+    () =>
+      (accountResources.data?.filter((item) => item.type.startsWith(ACCOUNT_TYPE.X1___COIN___COIN_STORE)) || []).sort((item) =>
+        isEqualsIgnoringCase(getCoinAddress(item.type), APTOS_COIN) ? -1 : 1,
+      ) as X1CoinCoinstore[],
+    [accountResources.data],
+  );
 
   return { currentAptosCoins };
 }
