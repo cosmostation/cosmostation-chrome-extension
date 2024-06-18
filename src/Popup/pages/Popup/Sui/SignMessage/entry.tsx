0faf8aab9cf9dff9672876746eeaca7b7601867f
SIhun Ahn <ahnsihun9208@gmail.com>
2024-06-18 11:28:16 +0900

    feat(ledger): Support sign sui:signMessage with ledger

--- src/Popup/pages/Popup/Sui/SignMessage/entry.tsx
+++ src/Popup/pages/Popup/Sui/SignMessage/entry.tsx
@@ -1,7 +1,18 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useSnackbar } from 'notistack';
 import { Typography } from '@mui/material';
-import { Connection, Ed25519Keypair, JsonRpcProvider, RawSigner } from '@mysten/sui.js';
+import Sui from '@mysten/ledgerjs-hw-app-sui';
+import {
+  Connection,
+  Ed25519Keypair,
+  Ed25519PublicKey,
+  IntentScope,
+  JsonRpcProvider,
+  messageWithIntent,
+  RawSigner,
+  toSerializedSignature,
+} from '@mysten/sui.js';
+import { bcs } from '@mysten/sui.js/bcs';
 
 import { SUI } from '~/constants/chain/sui/sui';
 import { RPC_ERROR, RPC_ERROR_MESSAGE } from '~/constants/error';
@@ -12,6 +23,8 @@ import { useCurrentAccount } from '~/Popup/hooks/useCurrent/useCurrentAccount';
 import { useCurrentPassword } from '~/Popup/hooks/useCurrent/useCurrentPassword';
 import { useCurrentQueue } from '~/Popup/hooks/useCurrent/useCurrentQueue';
 import { useCurrentSuiNetwork } from '~/Popup/hooks/useCurrent/useCurrentSuiNetwork';
+import { useLedgerTransport } from '~/Popup/hooks/useLedgerTransport';
+import { useLoading } from '~/Popup/hooks/useLoading';
 import { useTranslation } from '~/Popup/hooks/useTranslation';
 import { getAddress, getKeyPair } from '~/Popup/utils/common';
 import { responseToWeb } from '~/Popup/utils/message';
@@ -43,9 +56,14 @@ export default function Entry({ queue }: EntryProps) {
   const { currentPassword } = useCurrentPassword();
   const { enqueueSnackbar } = useSnackbar();
 
+  const { setLoadingLedgerSigning } = useLoading();
+  const { closeTransport, createTransport } = useLedgerTransport();
+
   const { currentSuiNetwork } = useCurrentSuiNetwork();
 
   const { t } = useTranslation();
+
+  const [isProgress, setIsProgress] = useState(false);
 
   const keyPair = getKeyPair(currentAccount, chain, currentPassword);
 
@@ -129,8 +147,11 @@ export default function Entry({ queue }: EntryProps) {
             {t('pages.Popup.Sui.SignMessage.entry.cancelButton')}
           </OutlineButton>
           <Button
+            isProgress={isProgress}
             onClick={async () => {
               try {
+                setIsProgress(true);
+
                 if (currentAccount.type === 'MNEMONIC' || currentAccount.type === 'PRIVATE_KEY') {
                   const keypair = Ed25519Keypair.fromSecretKey(keyPair!.privateKey!);
 
@@ -156,10 +177,46 @@ export default function Entry({ queue }: EntryProps) {
                 }
 
                 if (currentAccount.type === 'LEDGER') {
-                  throw new Error('not support');
+                  if (!keyPair?.publicKey) {
+                    throw new Error('key does not exist');
+                  }
+
+                  setLoadingLedgerSigning(true);
+                  const transport = await createTransport();
+                  const suiApp = new Sui(transport);
+
+                  const path = `${chain.bip44.purpose}/${chain.bip44.coinType}/${chain.bip44.account}/${chain.bip44.change}/${currentAccount.bip44.addressIndex}'`;
+
+                  const intentMessage = messageWithIntent(IntentScope.PersonalMessage, bcs.ser(['vector', 'u8'], encodedMessage).toBytes());
+
+                  const { signature } = await suiApp.signTransaction(path, intentMessage);
+
+                  const pubKey = new Ed25519PublicKey(keyPair.publicKey);
+
+                  const serializedSignature = toSerializedSignature({ signature, signatureScheme: 'ED25519', pubKey });
+
+                  const result = {
+                    signature: serializedSignature,
+                    messageBytes: params.message,
+                  };
+
+                  responseToWeb({
+                    response: {
+                      result,
+                    },
+                    message,
+                    messageId,
+                    origin,
+                  });
+
+                  await deQueue();
                 }
               } catch (e) {
                 enqueueSnackbar((e as { message: string }).message, { variant: 'error' });
+              } finally {
+                await closeTransport();
+                setLoadingLedgerSigning(false);
+                setIsProgress(false);
               }
             }}
           >
