93e64118c4e10c3400f2c640f061a9cac3dd18d3
SIhun Ahn <ahnsihun9208@gmail.com>
2024-07-24 18:51:30 +0900

    chore: Update Artela testnet chainId

--- src/constants/chain/ethereum/network/artelaTestnet.ts
+++ src/constants/chain/ethereum/network/artelaTestnet.ts
@@ -3,7 +3,7 @@ import type { EthereumNetwork } from '~/types/chain';
 
 export const ARTELA_TESTNET: EthereumNetwork = {
   id: 'ddf783bb-39ba-49f4-84f6-fbec338f6c0a',
-  chainId: '0x2E2E',
+  chainId: '0x2e2e',
   networkName: COSMOS_ARTELA_TESTNET.chainName,
   rpcURL: 'https://rpc-office-evm.cosmostation.io/artela-testnet/',
   tokenImageURL: COSMOS_ARTELA_TESTNET.tokenImageURL,
