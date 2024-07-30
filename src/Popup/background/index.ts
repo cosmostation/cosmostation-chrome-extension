ce74208acb6a4c50e1273c43adb8b6f7d2ae03b8
Sihun Ahn <87967564+G-Gamja@users.noreply.github.com>
2024-07-30 12:36:20 +0900

    [APIS-388] Add Agoric chain (#369)

--- src/Popup/background/index.ts
+++ src/Popup/background/index.ts
@@ -1,5 +1,6 @@
 import { APTOS_NETWORKS, ETHEREUM_NETWORKS, SUI_NETWORKS } from '~/constants/chain';
 import { APTOS } from '~/constants/chain/aptos/aptos';
+import { AGORIC } from '~/constants/chain/cosmos/agoric';
 import { COSMOS } from '~/constants/chain/cosmos/cosmos';
 import { ETHEREUM } from '~/constants/chain/ethereum/ethereum';
 import { BASE } from '~/constants/chain/ethereum/network/base';
@@ -96,7 +97,7 @@ function background() {
         const extensionManifest = chrome.runtime.getManifest();
         if (extensionManifest.version === '0.8.12') {
           void (async () => {
-            await setStorage('allowedChainIds', [...(await getStorage('allowedChainIds')), BASE.id]);
+            await setStorage('allowedChainIds', [...(await getStorage('allowedChainIds')), AGORIC.id, BASE.id]);
           })();
         }
       }
