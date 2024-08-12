c7bbfc808aedbffab1f772de62ff452663ce42a5
SIhun Ahn <ahnsihun9208@gmail.com>
2024-08-12 13:48:04 +0900

    chore: Add debouncing in search token

--- src/Popup/pages/Wallet/Swap/components/SwapCoinContainer/components/TokenListBottomSheet/index.tsx
+++ src/Popup/pages/Wallet/Swap/components/SwapCoinContainer/components/TokenListBottomSheet/index.tsx
@@ -1,9 +1,13 @@
 import { useEffect, useMemo, useRef, useState } from 'react';
+import { useDebounce } from 'use-debounce';
 import { InputAdornment, Typography } from '@mui/material';
 
 import { ETHEREUM, EVM_NATIVE_TOKEN_ADDRESS } from '~/constants/chain/ethereum/ethereum';
+import { THEME_TYPE } from '~/constants/theme';
+import EmptyAsset from '~/Popup/components/EmptyAsset';
 import IntersectionObserver from '~/Popup/components/IntersectionObserver';
 import { useTokensBalanceSWR } from '~/Popup/hooks/SWR/ethereum/useTokensBalanceSWR';
+import { useExtensionStorage } from '~/Popup/hooks/useExtensionStorage';
 import { useTranslation } from '~/Popup/hooks/useTranslation';
 import { gt } from '~/Popup/utils/big';
 import { isEqualsIgnoringCase } from '~/Popup/utils/string';
@@ -11,9 +15,22 @@ import type { EthereumToken } from '~/types/chain';
 import type { IntegratedSwapChain, IntegratedSwapToken } from '~/types/swap/asset';
 
 import TokenItem from './components/TokenItem';
-import { AssetList, Container, Header, HeaderTitle, StyledBottomSheet, StyledButton, StyledInput, StyledSearch20Icon } from './styled';
+import {
+  AssetList,
+  Container,
+  ContentContainer,
+  Header,
+  HeaderTitle,
+  StyledBottomSheet,
+  StyledButton,
+  StyledCircularProgress,
+  StyledInput,
+  StyledSearch20Icon,
+} from './styled';
 
 import Close24Icon from '~/images/icons/Close24.svg';
+import NoResultDarkIcon from '~/images/icons/NoResultDark.svg';
+import NoResultLightIcon from '~/images/icons/NoResultLight.svg';
 
 type TokenListBottomSheetProps = Omit<React.ComponentProps<typeof StyledBottomSheet>, 'children'> & {
   availableTokenList?: IntegratedSwapToken[];
@@ -30,30 +47,30 @@ export default function TokenListBottomSheet({
   onClose,
   ...remainder
 }: TokenListBottomSheetProps) {
+  const { extensionStorage } = useExtensionStorage();
+
   const { t } = useTranslation();
   const ref = useRef<HTMLButtonElement>(null);
   const topRef = useRef<HTMLDivElement>(null);
 
   const [viewLimit, setViewLimit] = useState(30);
 
-  useEffect(() => {
-    if (remainder.open) {
-      setTimeout(() => ref.current?.scrollIntoView({ behavior: 'smooth', block: 'center' }), 0);
-    }
-  }, [remainder.open]);
-
   const [search, setSearch] = useState('');
+
+  const [debouncedSearch, { isPending, flush }] = useDebounce(search, 300);
 
   const filteredTokenList = useMemo(
     () =>
-      search.length > 1
+      debouncedSearch.length > 1
         ? availableTokenList
             ?.filter((item) =>
-              [item.displayDenom, item.name, item.tokenAddressOrDenom].map((str) => str.toLowerCase()).some((str) => str.indexOf(search.toLowerCase()) > -1),
+              [item.displayDenom, item.name, item.tokenAddressOrDenom]
+                .map((str) => str.toLowerCase())
+                .some((str) => str.indexOf(debouncedSearch.toLowerCase()) > -1),
             )
             .slice(0, viewLimit) || []
         : availableTokenList?.slice(0, viewLimit) || [],
-    [availableTokenList, viewLimit, search],
+    [availableTokenList, viewLimit, debouncedSearch],
   );
 
   const { data: tokensBalance } = useTokensBalanceSWR({
@@ -84,12 +101,24 @@ export default function TokenListBottomSheet({
   }, [currentSelectedChain?.line, filteredTokenList, tokensBalance]);
 
   useEffect(() => {
-    if (search.length > 1) {
+    if (remainder.open) {
+      setTimeout(() => ref.current?.scrollIntoView({ behavior: 'smooth', block: 'center' }), 0);
+    }
+  }, [remainder.open]);
+
+  useEffect(() => {
+    if (debouncedSearch.length > 1) {
       setTimeout(() => topRef.current?.scrollIntoView(), 0);
 
       setViewLimit(30);
     }
-  }, [search.length]);
+  }, [debouncedSearch.length]);
+
+  useEffect(() => {
+    if (search === '') {
+      flush();
+    }
+  }, [flush, search]);
 
   return (
     <StyledBottomSheet
@@ -131,33 +160,48 @@ export default function TokenListBottomSheet({
             setSearch(event.currentTarget.value);
           }}
         />
-        <AssetList>
-          <div ref={topRef} />
-          {sortedTokenList?.map((item) => {
-            const isActive = isEqualsIgnoringCase(item.tokenAddressOrDenom, currentSelectedToken?.tokenAddressOrDenom);
-            return (
-              <TokenItem
-                isActive={isActive}
-                key={item.tokenAddressOrDenom}
-                ref={isActive ? ref : undefined}
-                tokenInfo={item}
-                onClickToken={(clickedToken) => {
-                  onClickToken(clickedToken);
-                  setSearch('');
+        {isPending() && search !== '' ? (
+          <ContentContainer>
+            <StyledCircularProgress size="2.8rem" />
+          </ContentContainer>
+        ) : sortedTokenList?.length > 0 ? (
+          <AssetList>
+            <div ref={topRef} />
+            {sortedTokenList?.map((item) => {
+              const isActive = isEqualsIgnoringCase(item.tokenAddressOrDenom, currentSelectedToken?.tokenAddressOrDenom);
+              return (
+                <TokenItem
+                  isActive={isActive}
+                  key={item.tokenAddressOrDenom}
+                  ref={isActive ? ref : undefined}
+                  tokenInfo={item}
+                  onClickToken={(clickedToken) => {
+                    onClickToken(clickedToken);
+                    setSearch('');
 
-                  onClose?.({}, 'escapeKeyDown');
+                    onClose?.({}, 'escapeKeyDown');
+                  }}
+                />
+              );
+            })}
+
+            {filteredTokenList?.length > viewLimit - 1 && (
+              <IntersectionObserver
+                onIntersect={() => {
+                  setViewLimit((limit) => limit + 30);
                 }}
               />
-            );
-          })}
-          {filteredTokenList?.length > viewLimit - 1 && (
-            <IntersectionObserver
-              onIntersect={() => {
-                setViewLimit((limit) => limit + 30);
-              }}
+            )}
+          </AssetList>
+        ) : (
+          <ContentContainer>
+            <EmptyAsset
+              Icon={extensionStorage.theme === THEME_TYPE.LIGHT ? NoResultLightIcon : NoResultDarkIcon}
+              headerText={t('pages.Wallet.Swap.components.SwapCoinContainer.components.TokenListBottomSheet.index.noResult')}
+              subHeaderText={t('pages.Wallet.Swap.components.SwapCoinContainer.components.TokenListBottomSheet.index.noResultSub')}
             />
-          )}
-        </AssetList>
+          </ContentContainer>
+        )}
       </Container>
     </StyledBottomSheet>
   );
--- src/Popup/pages/Wallet/Swap/components/SwapCoinContainer/components/TokenListBottomSheet/styled.tsx
+++ src/Popup/pages/Wallet/Swap/components/SwapCoinContainer/components/TokenListBottomSheet/styled.tsx
@@ -1,3 +1,4 @@
+import { CircularProgress } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 import BottomSheet from '~/Popup/components/common/BottomSheet';
@@ -6,7 +7,9 @@ import Input from '~/Popup/components/common/Input';
 import Search20Icon from '~/images/icons/Search20.svg';
 
 export const Container = styled('div')({
-  padding: '1.6rem',
+  height: '100%',
+
+  padding: '1.6rem 1.6rem 0',
   overflow: 'hidden',
 
   display: 'flex',
@@ -49,7 +52,7 @@ export const AssetList = styled('div')({
 
 export const StyledBottomSheet = styled(BottomSheet)({
   '& .MuiPaper-root': {
-    maxHeight: '44rem',
+    height: '54.8rem',
 
     width: '36rem',
 
@@ -69,5 +72,22 @@ export const StyledButton = styled('button')(({ theme }) => ({
 
   '& > svg': {
     fill: theme.colors.base05,
+  },
+}));
+
+export const ContentContainer = styled('div')({
+  width: '100%',
+  height: '100%',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+export const StyledCircularProgress = styled(CircularProgress)(({ theme }) => ({
+  marginLeft: '0.4rem',
+
+  '&.MuiCircularProgress-root': {
+    color: theme.accentColors.purple01,
   },
 }));
