8fc4c40d0b22bf108a324b8cbfe6561901350cc1
SIhun Ahn <ahnsihun9208@gmail.com>
2024-08-21 13:53:05 +0900

    chore: Update SignMessage component to handle error boundary and suspense

--- src/Popup/pages/Popup/Sui/SignMessage/index.tsx
+++ src/Popup/pages/Popup/Sui/SignMessage/index.tsx
@@ -1,7 +1,13 @@
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+
+import { SUI } from '~/constants/chain/sui/sui';
+import ErrorPage from '~/Popup/components/ErrorPage';
 import Lock from '~/Popup/components/Lock';
 import AccessRequest from '~/Popup/components/requests/AccessRequest';
 import LedgerPublicKeyRequest from '~/Popup/components/requests/LedgerPublicKeyRequest';
 import { useCurrentQueue } from '~/Popup/hooks/useCurrent/useCurrentQueue';
+import { useCurrentSuiNetwork } from '~/Popup/hooks/useCurrent/useCurrentSuiNetwork';
 import type { Queue } from '~/types/extensionStorage';
 import type { SuiSignMessage } from '~/types/message/sui';
 
@@ -10,6 +16,7 @@ import Layout from './layout';
 
 export default function SignMessage() {
   const { currentQueue } = useCurrentQueue();
+  const { currentSuiNetwork } = useCurrentSuiNetwork();
 
   if (currentQueue && isSignMessage(currentQueue)) {
     return (
@@ -17,7 +24,14 @@ export default function SignMessage() {
         <LedgerPublicKeyRequest>
           <AccessRequest>
             <Layout>
-              <Entry queue={currentQueue} />
+              <ErrorBoundary
+                // eslint-disable-next-line react/no-unstable-nested-components
+                FallbackComponent={(props) => <ErrorPage queue={currentQueue} chain={SUI} network={currentSuiNetwork} {...props} />}
+              >
+                <Suspense fallback={null}>
+                  <Entry queue={currentQueue} />
+                </Suspense>
+              </ErrorBoundary>
             </Layout>
           </AccessRequest>
         </LedgerPublicKeyRequest>
