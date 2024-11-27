1e70e84ecefcf17832ed947b7b09de3f7090cc09
ong-ar <reclusis@gmail.com>
2024-11-27 11:33:18 +0900

    split vite config

---
+++ vite.config.chrome.service-worker.ts
@@ -0,0 +1,44 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+
+  const outDir = isProduction ? 'dist' : 'dist-dev';
+
+  const watch = isProduction
+    ? null
+    : {
+        include: ['src/script/service-worker/**'],
+      };
+
+  return {
+    define: {
+      __APP_BROWSER__: JSON.stringify('chrome'),
+      __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+      __APP_MODE__: JSON.stringify(mode),
+    },
+    plugins: [tsconfigPaths({ configNames: ['tsconfig.app.json'] }), nodePolyfills()],
+    build: {
+      outDir,
+      emptyOutDir: false,
+      watch,
+      sourcemap: !isProduction,
+      rollupOptions: {
+        input: {
+          service_worker: resolve(__dirname, 'src/script/service-worker/index.ts'),
+        },
+        output: [
+          {
+            entryFileNames: 'js/[name].js',
+            assetFileNames: 'assets/[name]-[hash].[ext]',
+            inlineDynamicImports: true,
+            format: 'es',
+          },
+        ],
+      },
+    },
+  };
+});
--- vite.config.chrome.ts
+++ vite.config.chrome.ts
@@ -51,15 +51,13 @@ export default defineConfig(({ mode }) => {
     ],
     build: {
       outDir,
+      emptyOutDir: false,
       watch,
       sourcemap: !isProduction,
       rollupOptions: {
         input: {
           popup: resolve(__dirname, 'popup.html'),
           sidepanel: resolve(__dirname, 'sidepanel.html'),
-          service_worker: resolve(__dirname, 'src/script/service-worker/index.ts'),
-          inject: resolve(__dirname, 'src/script/inject/index.ts'),
-          content: resolve(__dirname, 'src/script/content/index.ts'),
         },
         output: [
           {
