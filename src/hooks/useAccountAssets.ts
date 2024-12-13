9e22a302fc083305f323a6328c13dcca684cd875
SIhun Ahn <ahnsihun9208@gmail.com>
2024-12-13 20:20:37 +0900

    feat: useAccountAssets 훅에 accountId 및 config 매개변수 추가, 쿼리 키 및 스토리지 갱신 로직 수정

--- src/hooks/useAccountAssets.ts
+++ src/hooks/useAccountAssets.ts
@@ -1,18 +1,29 @@
+import type { UseQueryOptions } from '@tanstack/react-query';
 import { useQuery } from '@tanstack/react-query';
 
 import { getAccountAssets } from '@/libs/asset';
 import { sendMessage } from '@/libs/extension';
+import type { AccountAssets } from '@/types/accountAssets';
 
 import { useCurrentAccount } from './useCurrentAccount';
 
-export function useAccountAssets() {
+type UseAccountAssets =
+  | {
+      accountId?: string;
+      config?: UseQueryOptions<AccountAssets | null>;
+    }
+  | undefined;
+
+export function useAccountAssets({ accountId, config }: UseAccountAssets = {}) {
   const { currentAccount } = useCurrentAccount();
+
+  const param = accountId || currentAccount.id;
 
   const fetcher = async () => {
     try {
       // NOTE 스토리지 갱신 로직 이 자리에 추가. -> 갱신 생애주기가 살아있을때만 갱신.
-      await sendMessage({ target: 'SERVICE_WORKER', method: 'updateBalance', params: [currentAccount.id] });
-      return await getAccountAssets(currentAccount.id);
+      await sendMessage({ target: 'SERVICE_WORKER', method: 'updateBalance', params: [param] });
+      return await getAccountAssets(param);
     } catch {
       return null;
     }
@@ -24,11 +35,12 @@ export function useAccountAssets() {
     error,
     refetch,
   } = useQuery({
-    queryKey: ['accountAssets', currentAccount.id],
+    queryKey: ['accountAssets', param],
     queryFn: fetcher,
-    enabled: !!currentAccount.id,
+    enabled: !!param,
     staleTime: 1000 * 60 * 5,
     refetchInterval: 1000 * 60 * 10,
+    ...config,
   });
 
   return { currentAccountAssets, isLoading, error, refetch };
