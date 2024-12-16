cfcca88ba87eee2554979c4497e1172b416bc480
SIhun Ahn <ahnsihun9208@gmail.com>
2024-12-16 18:39:11 +0900

    fix: 카바 evm decimal 이슈 해결(하드코딩)

--- src/libs/asset.ts
+++ src/libs/asset.ts
@@ -39,6 +39,18 @@ export async function getAssets() {
   );
 
   const evmAssets: EvmAsset[] = filteredEvmAssets.map((asset) => {
+    // TODO 하드코딩 제거
+    if (asset.chain === 'kava') {
+      return {
+        ...asset,
+        type: 'native',
+        id: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+        chainId: asset.chain,
+        chainType: 'evm',
+        decimals: 18,
+      };
+    }
+
     return {
       ...asset,
       type: 'native',
