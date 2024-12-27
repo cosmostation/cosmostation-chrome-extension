c5aecf88d9e66255fbb78f078767735b7c7b8299
SIhun Ahn <ahnsihun9208@gmail.com>
2024-12-27 17:26:57 +0900

    feat: 다양한 블록체인 주소 및 트랜잭션 해시를 검증하는 정규 표현식 추가

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,1 +1,23 @@
+export const ethereumAddressRegex = /^0x[a-fA-F0-9]{40}$/;
+
+export const aptosAddressRegex = /^0x([a-fA-F0-9]{64}|[a-fA-F1-9]{1}[a-fA-F0-9]{62})$/;
+
+export const suiAddressRegex = /^0x([a-fA-F0-9]{64}|[a-fA-F1-9]{1}[a-fA-F0-9]{62})$/;
+
+export const getCosmosAddressRegex = (prefix: string, lengths: number[]) =>
+  new RegExp(`^${prefix}(${lengths.map((item) => `(.{${item},${item}})`).join('|')})$`);
+
+export const hexRegex = /^0[x][0-9a-f]+$/;
+export const hexOrDecRegex = /^(0[x][0-9a-f]+|[0-9]+)$/;
+
+export const httpsRegex = /^https:\/\/[\w-]+(\.[\w-]+)*(:\d+)?(\/\S*)?$/;
+
 export const isNaturalNumberRegex = /^\d+$/;
+
+export const cosmosTxHashRegex = /^[0-9A-Fa-f]{64}/;
+
+export const ethereumTxHashRegex = /^0x([A-Fa-f0-9]{64})$/;
+
+export const suiTxHashRegex = /^[A-Za-z0-9]{44}/;
+
+export const aptosTxHashRegex = /^0x([A-Fa-f0-9]{64})$/;
