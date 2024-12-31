c9ff1dded589f9a28a88c0a7f80a25279ac469b0
SIhun Ahn <ahnsihun9208@gmail.com>
2024-12-31 18:09:29 +0900

    feat: 네트워크 자산 가져오기 페이지의 레이아웃 및 엔트리 컴포넌트 추가

---
+++ src/pages/manage-assets/import/network/-entry.tsx
@@ -0,0 +1,13 @@
+// import { useTranslation } from 'react-i18next';
+
+import BaseBody from '@/components/BaseLayout/components/BaseBody';
+
+export default function Entry() {
+  // const { t } = useTranslation();
+
+  return (
+    <BaseBody>
+      <>test</>
+    </BaseBody>
+  );
+}
--- src/pages/manage-assets/import/network/index.tsx
+++ src/pages/manage-assets/import/network/index.tsx
@@ -1,10 +1,17 @@
 import { createFileRoute } from '@tanstack/react-router';
 
+import Entry from './-entry';
+import Layout from './-layout';
+
 export const Route = createFileRoute('/manage-assets/import/network/')({
-  component: RouteComponent,
+  component: ImportCustomNetwork,
 });
 
 // NOTE 체인 추가 시 기본 메인 코인은 visible도 같이 추가되도록 작업.
-function RouteComponent() {
-  return <div>Hello &quot;/manage-assets/import/network/&quot;!</div>;
+function ImportCustomNetwork() {
+  return (
+    <Layout>
+      <Entry />
+    </Layout>
+  );
 }
