beb279d80f8926fffed8581950698c23839eddb6
SIhun Ahn <ahnsihun9208@gmail.com>
2025-01-07 19:27:36 +0900

    feat: receive 페이지에서 모든 코인 및 체인 선택 가능하도록 주석 추가

--- src/pages/wallet/receive/-entry.tsx
+++ src/pages/wallet/receive/-entry.tsx
@@ -5,6 +5,7 @@ import EdgeAligner from '@/components/BaseLayout/components/EdgeAligner';
 import CoinSelect from '@/components/CoinSelect';
 import { Route as Receive } from '@/pages/wallet/receive/$coinId';
 
+// NOTE !! receive에서는 히든처리 안된 모든 코인,체인에 대해서 선택이 가능하고 또 주소를 볼 수 있어야한다.
 export default function Entry() {
   const navigate = useNavigate();
 
