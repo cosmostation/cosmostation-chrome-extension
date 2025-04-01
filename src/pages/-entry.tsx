f47677c54902d2f1720e841a97e9a79357d20f2e
SIhun Ahn <ahnsihun9208@gmail.com>
2025-04-01 20:13:23 +0900

    feat: 기존 포트폴리오 토큰 리스팅 로직 롤백 & sort 참조문제 픽스

--- src/pages/-entry.tsx
+++ src/pages/-entry.tsx
@@ -115,32 +115,47 @@ export default function Entry() {
     search,
   ]);
 
-  const filteredSortedAssets = useMemo(() => {
-    const visibleAssets = isBalanceVisible ? computedAssetValues : computedAssetValues.filter((coin) => gte(coin.value, '0.001'));
-
-    const sortedAssets = [...visibleAssets].sort((a, b) => {
-      if (dashboardCoinSortKey === DASHBOARD_COIN_SORT_KEY.VALUE_HIGH_ORDER) {
-        return Number(minus(b.value, a.value));
-      }
-      if (dashboardCoinSortKey === DASHBOARD_COIN_SORT_KEY.ALPHABETICAL_ASC) {
-        return a.asset.symbol.localeCompare(b.asset.symbol);
-      }
-      return 0;
-    });
-
-    const filteredByChain = getFilteredAssetsByChainId(sortedAssets, currentSelectedChainId);
-
-    if (!!search && debouncedSearch.length > 1) {
-      return filteredByChain
-        .filter((asset) => {
-          const condition = [asset.asset.symbol, asset.asset.id];
-          return condition.some((item) => item.toLowerCase().includes(debouncedSearch.toLowerCase()));
-        })
-        .slice(0, viewLimit);
+  const hideSmallValueAssets = useMemo(() => {
+    if (!isBalanceVisible) {
+      return computedAssetValues.filter((coin) => {
+        return gte(coin.value, '0.001');
+      });
     }
 
-    return filteredByChain.slice(0, viewLimit);
-  }, [computedAssetValues, isBalanceVisible, dashboardCoinSortKey, currentSelectedChainId, debouncedSearch, search, viewLimit]);
+    return computedAssetValues;
+  }, [computedAssetValues, isBalanceVisible]);
+
+  const sortedAssets = useMemo(
+    () =>
+      hideSmallValueAssets.toSorted((a, b) => {
+        if (dashboardCoinSortKey === DASHBOARD_COIN_SORT_KEY.VALUE_HIGH_ORDER) {
+          return Number(minus(b.value, a.value));
+        }
+
+        if (dashboardCoinSortKey === DASHBOARD_COIN_SORT_KEY.ALPHABETICAL_ASC) {
+          return a.asset.symbol.localeCompare(b.asset.symbol);
+        }
+
+        return 0;
+      }),
+    [dashboardCoinSortKey, hideSmallValueAssets],
+  );
+
+  const filteredAssetsBySearch = useMemo(() => {
+    const filterdByChain = getFilteredAssetsByChainId(sortedAssets, currentSelectedChainId);
+    if (!!search && debouncedSearch.length > 1) {
+      return (
+        filterdByChain
+          .filter((asset) => {
+            const condition = [asset.asset.symbol, asset.asset.id];
+
+            return condition.some((item) => item.toLowerCase().indexOf(debouncedSearch.toLowerCase()) > -1);
+          })
+          .slice(0, viewLimit) || []
+      );
+    }
+    return filterdByChain.slice(0, viewLimit);
+  }, [currentSelectedChainId, debouncedSearch, search, sortedAssets, viewLimit]);
 
   const handleChange = (_: React.SyntheticEvent, newTabValue: number) => {
     setTabValue(newTabValue);
@@ -225,10 +240,10 @@ export default function Entry() {
                 </ManageCryptoContainer>
               </StickyTabPanelContentsContainer>
               <CoinButtonWrapper>
-                {isLoading || (filteredSortedAssets && filteredSortedAssets.length === 0) ? (
+                {isLoading || (filteredAssetsBySearch && filteredAssetsBySearch.length === 0) ? (
                   <SkeletonCoinList />
                 ) : (
-                  filteredSortedAssets.map((coin) => {
+                  filteredAssetsBySearch.map((coin) => {
                     const destinationRoute = coin.counts && gt(coin.counts, '1') ? CoinOverview.to : CoinDetail.to;
 
                     const isGroupToken = gt(coin.counts || '0', '1');
@@ -256,7 +271,7 @@ export default function Entry() {
                     );
                   })
                 )}
-                {(filteredSortedAssets?.length || 0) > viewLimit - 1 && (
+                {(filteredAssetsBySearch?.length || 0) > viewLimit - 1 && (
                   <IntersectionObserver
                     onIntersect={() => {
                       setViewLimit((limit) => limit + 30);
