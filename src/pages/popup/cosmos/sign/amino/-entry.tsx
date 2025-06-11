64e63dcb4b72227cb4ec1f0e9957351639b4ea6e
SIhun Ahn <ahnsihun9208@gmail.com>
2025-06-11 15:43:56 +0900

    feat: reflect dapp select fee coin in alternativeFeeAsset

--- src/pages/popup/cosmos/sign/amino/-entry.tsx
+++ src/pages/popup/cosmos/sign/amino/-entry.tsx
@@ -121,10 +121,24 @@ export default function Entry({ request, chain }: EntryProps) {
 
   const [customFeeCoinId, setCustomFeeCoinId] = useState('');
 
-  const alternativeFeeAsset = useMemo(
-    () => (customFeeCoinId ? feeAssets.find((item) => isMatchingCoinId(item.asset, customFeeCoinId)) : feeAssets[0]),
-    [customFeeCoinId, feeAssets],
-  );
+  const alternativeFeeAsset = useMemo(() => {
+    if (customFeeCoinId) {
+      return feeAssets.find((item) => isMatchingCoinId(item.asset, customFeeCoinId));
+    }
+
+    const extensionSelected = feeAssets[0];
+
+    if (dappFromFeeAsset) {
+      const dappSelectedFeeCoinId = getCoinId(dappFromFeeAsset.asset);
+      const extensionSelectedFeeCoinId = getCoinId(extensionSelected.asset);
+
+      if (dappSelectedFeeCoinId !== extensionSelectedFeeCoinId) {
+        return feeAssets.find((item) => getCoinId(item.asset) === dappSelectedFeeCoinId) || extensionSelected;
+      }
+    }
+
+    return extensionSelected;
+  }, [customFeeCoinId, dappFromFeeAsset, feeAssets]);
 
   const alternativeFeeCoinId = useMemo(() => (alternativeFeeAsset?.asset ? getCoinId(alternativeFeeAsset.asset) : ''), [alternativeFeeAsset?.asset]);
 
