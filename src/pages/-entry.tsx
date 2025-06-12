1f996c2816c7f93f4c3c3703f41150ec762d4884
SIhun Ahn <ahnsihun9208@gmail.com>
2025-06-12 18:37:22 +0900

    feat: improve listing default coins compare logic

--- src/pages/-entry.tsx
+++ src/pages/-entry.tsx
@@ -33,6 +33,7 @@ import { getFilteredAssetsByChainId, isStakeableAsset } from '@/utils/asset';
 import { isTestnetChain } from '@/utils/chain';
 import { gt, gte, minus, times, toDisplayDenomAmount } from '@/utils/numbers';
 import { getCoinId } from '@/utils/queryParamGenerator';
+import { isEqualsIgnoringCase } from '@/utils/string';
 import { useExtensionStorageStore } from '@/zustand/hooks/useExtensionStorageStore';
 
 import NFTList from './-components/NFTList';
@@ -94,16 +95,16 @@ export default function Entry() {
     const chainDefaultCoins = chainFilteredAllCoins
       .filter((item) => {
         if (item.chain.chainDefaultCoinDenoms) {
-          return item.chain.chainDefaultCoinDenoms.includes(item.asset.id);
+          return item.chain.chainDefaultCoinDenoms.some((defaultCoinDenom) => isEqualsIgnoringCase(defaultCoinDenom, item.asset.id));
         }
 
-        return item.chain.mainAssetDenom === item.asset.id;
+        return isEqualsIgnoringCase(item.chain.mainAssetDenom || undefined, item.asset.id);
       })
       .toSorted((a, b) => {
         const denoms = a.chain.chainDefaultCoinDenoms;
         if (!denoms) return 0;
-        const indexA = denoms.indexOf(a.asset.id);
-        const indexB = denoms.indexOf(b.asset.id);
+        const indexA = denoms.findIndex((d) => isEqualsIgnoringCase(d, a.asset.id));
+        const indexB = denoms.findIndex((d) => isEqualsIgnoringCase(d, b.asset.id));
         return indexA - indexB;
       });
 
