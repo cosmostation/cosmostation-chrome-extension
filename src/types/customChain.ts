cf78f1748c0be0af75e9426678d67d8444d0c1b7
SIhun Ahn <ahnsihun9208@gmail.com>
2025-06-19 14:16:46 +0900

    refactor: make chainDefaultCoinDenoms to null possible

--- src/types/customChain.ts
+++ src/types/customChain.ts
@@ -88,7 +88,7 @@ export interface CustomEvmChainAsset extends ChainBase {
   mainAssetDecimals: number;
   mainAssetImage: string | null;
   mainAssetCoinGeckoId: string | null;
-  chainDefaultCoinDenoms: string[];
+  chainDefaultCoinDenoms?: string[] | null;
   isCosmos: boolean;
   feeInfo: EvmFeeInfo;
   rpcUrls: ChainEndpoint[];
@@ -104,7 +104,7 @@ export interface CustomCosmosChainAsset extends ChainBase {
   mainAssetDecimals: number;
   mainAssetImage: string | null;
   mainAssetCoinGeckoId: string | null;
-  chainDefaultCoinDenoms: string[];
+  chainDefaultCoinDenoms?: string[] | null;
   accountPrefix: string;
   isCosmwasm: boolean;
   isEvm: boolean;
