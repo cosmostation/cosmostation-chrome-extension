e701cdc07ee7915fbc46e3d07f1985cdd47707ad
SIhun Ahn <ahnsihun9208@gmail.com>
2025-07-02 14:27:15 +0900

    minor: change RATE_LIMIT_MS 3 min to 1 min

--- src/constants/updateRequest.ts
+++ src/constants/updateRequest.ts
@@ -1,5 +1,5 @@
 export const RATE_LIMIT_MS = {
-  updateBalance: 3 * 60 * 1000,
-  updateStaking: 3 * 60 * 1000,
-  updateAccountInfo: 3 * 60 * 1000,
+  updateBalance: 1 * 60 * 1000,
+  updateStaking: 1 * 60 * 1000,
+  updateAccountInfo: 1 * 60 * 1000,
 } as const;
