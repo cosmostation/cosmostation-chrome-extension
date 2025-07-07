2b75a08bd66bc347b7163c91021d312e7f2ff445
SIhun Ahn <ahnsihun9208@gmail.com>
2025-07-07 16:34:16 +0900

    refactor: move call startPostHydrationWorks position after Lock

--- src/components/Wrapper/components/Init/index.tsx
+++ src/components/Wrapper/components/Init/index.tsx
@@ -6,7 +6,6 @@ import type { ExtensionStorageKeys } from '@/types/extension';
 import type { LanguageType } from '@/types/language';
 import { extension } from '@/utils/browser';
 import { getExtensionLocalStorage, initExtensionLocalStorage, setExtensionLocalStorage } from '@/utils/storage';
-import { checkMissingAddresses } from '@/utils/storageSync/newChain';
 import { loadExtensionSessionStorageStoreFromStorage } from '@/zustand/hooks/useExtensionSessionStorageStore';
 import { loadAllStoreFromStorage } from '@/zustand/utils';
 
@@ -63,7 +62,6 @@ export default function Init({ children }: InitProps) {
       await loadAllStoreFromStorage();
 
       setIsHydrated(true);
-      startPostHydrationWorks();
     })();
 
     return () => {
@@ -77,8 +75,4 @@ export default function Init({ children }: InitProps) {
   }
 
   return <>{children}</>;
-}
-
-function startPostHydrationWorks() {
-  void checkMissingAddresses();
 }
