a880909f80fe5e37e6f62500c512613ffc32c853
SIhun Ahn <ahnsihun9208@gmail.com>
2025-11-07 14:52:10 +0900

    refactor: balance visible control

--- src/components/MainBox/Portfolio/components/BalanceValueButton/index.tsx
+++ src/components/MainBox/Portfolio/components/BalanceValueButton/index.tsx
@@ -1,12 +1,12 @@
 import Typography from '@mui/material/Typography';
 
 import BalanceDisplay from '@/components/BalanceDisplay';
-import IconTextButton from '@/components/common/IconTextButton';
 import { useCoinGeckoPrice } from '@/hooks/useCoinGeckoPrice';
 import type { PortfolioCoinItem } from '@/pages/-entry';
 import { plus } from '@/utils/numbers';
 import { useExtensionStorageStore } from '@/zustand/hooks/useExtensionStorageStore';
 
+import { Container, StyledIconButton, ValueButton } from './styled';
 import { StyledIconContainer, TotalBalanceContainer } from '../../styled';
 
 import RefreshIcon from '@/assets/images/icons/Refresh18.svg';
@@ -14,20 +14,14 @@ import RefreshIcon from '@/assets/images/icons/Refresh18.svg';
 interface BalanceValueButtonProps {
   accountAssets: PortfolioCoinItem[];
   isUpdatingBalance: boolean;
-  isHovering: boolean;
   handleManualBalanceUpdate: () => void;
-  handleHovering: (value: boolean) => void;
 }
 
-export default function BalanceValueButton({
-  accountAssets,
-  isUpdatingBalance,
-  isHovering,
-  handleManualBalanceUpdate,
-  handleHovering,
-}: BalanceValueButtonProps) {
+export default function BalanceValueButton({ accountAssets, isUpdatingBalance, handleManualBalanceUpdate }: BalanceValueButtonProps) {
   const { isLoading } = useCoinGeckoPrice();
   const userCurrencyPreference = useExtensionStorageStore((state) => state.userCurrencyPreference);
+  const isBalanceVisible = useExtensionStorageStore((state) => state.isBalanceVisible);
+  const updateExtensionStorageStore = useExtensionStorageStore((state) => state.updateExtensionStorageStore);
 
   const aggregateValue = accountAssets.reduce((acc, cur) => {
     const sum = plus(acc, cur.value);
@@ -36,36 +30,36 @@ export default function BalanceValueButton({
   }, '0');
 
   return (
-    <IconTextButton
-      onClick={handleManualBalanceUpdate}
-      onMouseEnter={() => {
-        handleHovering(true);
-      }}
-      onMouseLeave={() => {
-        handleHovering(false);
-      }}
-      isHovering={isHovering}
-      disabled={isUpdatingBalance}
-      trailingIcon={
+    <Container>
+      <ValueButton
+        onClick={() => {
+          updateExtensionStorageStore('isBalanceVisible', !isBalanceVisible);
+        }}
+      >
+        <TotalBalanceContainer>
+          {!accountAssets || accountAssets.length === 0 || isLoading ? (
+            <Typography variant="h1n_B">{'--'}</Typography>
+          ) : (
+            <BalanceDisplay typoOfIntegers="h1n_B" typoOfDecimals="h2n_M" currency={userCurrencyPreference} isDisableLeadingCurreny>
+              {aggregateValue}
+            </BalanceDisplay>
+          )}
+          &nbsp;
+          <Typography variant="h2_M">{userCurrencyPreference.toLocaleUpperCase()}</Typography>
+        </TotalBalanceContainer>
+      </ValueButton>
+
+      <StyledIconButton
+        onClick={isUpdatingBalance ? undefined : handleManualBalanceUpdate}
+        sx={{
+          opacity: isUpdatingBalance ? 0.7 : 1,
+          cursor: isUpdatingBalance ? 'not-allowed' : 'pointer',
+        }}
+      >
         <StyledIconContainer data-is-loading={isUpdatingBalance}>
           <RefreshIcon />
         </StyledIconContainer>
-      }
-      style={{
-        cursor: isUpdatingBalance ? 'not-allowed' : 'default',
-      }}
-    >
-      <TotalBalanceContainer>
-        {!accountAssets || accountAssets.length === 0 || isLoading ? (
-          <Typography variant="h1n_B">{'--'}</Typography>
-        ) : (
-          <BalanceDisplay typoOfIntegers="h1n_B" typoOfDecimals="h2n_M" currency={userCurrencyPreference} isDisableLeadingCurreny>
-            {aggregateValue}
-          </BalanceDisplay>
-        )}
-        &nbsp;
-        <Typography variant="h2_M">{userCurrencyPreference.toLocaleUpperCase()}</Typography>
-      </TotalBalanceContainer>
-    </IconTextButton>
+      </StyledIconButton>
+    </Container>
   );
 }
