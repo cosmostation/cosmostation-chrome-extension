fbd15e46ce5a5b2beddd693cb610a3785d914e4e
SIhun Ahn <ahnsihun9208@gmail.com>
2025-11-19 18:49:48 +0900

    fix: wrong type casting

--- src/utils/validation/address.ts
+++ src/utils/validation/address.ts
@@ -3,7 +3,7 @@ import { isValidAddress } from 'ethereumjs-util';
 import { isValidIotaAddress } from '@iota/iota-sdk/utils';
 import { isValidSuiAddress } from '@mysten/sui/utils';
 
-import type { BitcoinChain, ChainBase, ChainType, CosmosChain } from '@/types/chain';
+import type { BitcoinChain, ChainBase, ChainType, CosmosChain, GnoChain } from '@/types/chain';
 
 import { isValidAptosAddress } from '../aptos/validation';
 import { isValidCosmosAddress } from '../cosmos/address';
@@ -44,7 +44,7 @@ const addressValidators: Record<ChainType, AddressValidator> = {
   },
 
   gno: (address, chain) => {
-    const chainCasted = chain as CosmosChain;
+    const chainCasted = chain as GnoChain;
     return isValidCosmosAddress(address, chainCasted.accountPrefix);
   },
 };
