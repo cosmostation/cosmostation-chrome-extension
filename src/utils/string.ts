983b88aa93248fd9750b092105aa2706813aae31
SIhun Ahn <ahnsihun9208@gmail.com>
2025-11-20 17:48:17 +0900

    minor: wrap errorStringify with try/catch

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -187,32 +187,36 @@ export function safeStringify(value: unknown, space = 4) {
 }
 
 export function errorStringify(error: unknown, path?: string): string {
-  if (!error) {
+  try {
+    if (!error) {
+      return 'No error information available';
+    }
+
+    const normalizedError =
+      error instanceof Error
+        ? {
+            ...Object.getOwnPropertyNames(error).reduce(
+              (acc, key) => {
+                try {
+                  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                  acc[key] = (error as any)[key];
+                } catch {
+                  acc[key] = '[Getter Error]';
+                }
+                return acc;
+              },
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+              {} as Record<string, any>,
+            ),
+            name: error.name,
+            message: error.message,
+            stack: error.stack,
+            path: path || 'unknown',
+          }
+        : error;
+
+    return safeStringify(normalizedError) ?? String(error);
+  } catch {
     return 'No error information available';
   }
-
-  const normalizedError =
-    error instanceof Error
-      ? {
-          ...Object.getOwnPropertyNames(error).reduce(
-            (acc, key) => {
-              try {
-                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                acc[key] = (error as any)[key];
-              } catch {
-                acc[key] = '[Getter Error]';
-              }
-              return acc;
-            },
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            {} as Record<string, any>,
-          ),
-          name: error.name,
-          message: error.message,
-          stack: error.stack,
-          path: path || 'unknown',
-        }
-      : error;
-
-  return safeStringify(normalizedError) ?? String(error);
 }
