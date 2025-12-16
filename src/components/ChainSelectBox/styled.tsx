f5f6a887debd20d602055a236ec892599dcb6ce3
SIhun Ahn <ahnsihun9208@gmail.com>
2025-12-16 16:33:41 +0900

    style: fix ChainSelectBox ui error in firefox

--- src/components/ChainSelectBox/styled.tsx
+++ src/components/ChainSelectBox/styled.tsx
@@ -134,7 +134,6 @@ export const HelperTextContainer = styled('div')<HelperTextContainerProps>(({ th
 
 export const ChainImageContainer = styled(BaseChainImage)({
   width: '2.2rem',
-  height: '2.2rem',
   paddingBottom: '0.5rem',
 });
 
